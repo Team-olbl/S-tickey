2e8c426825625d2bcb517112bc9bb4b6b847967a
윤희원 <gmldnjs427@naver.com>
2024-03-14 17:29:27 +0900

    [BE] init: backend 프로젝트 생성 #S10P22D211-105 (# done)

---
+++ stickey-gateway/src/main/java/com/olbl/stickeygateway/StickeyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.olbl.stickeygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickeyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickeyGatewayApplication.class, args);
+	}
+
+}
---
+++ stickey-gateway/src/test/java/com/olbl/stickeygateway/StickeyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olbl.stickeygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickeyGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
