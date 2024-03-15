247fcc67447284d979f7676758949e107246ded3
강정수 <bbbbqz@nate.com>
2024-03-15 09:40:02 +0900

    [FE] init:초기세팅 #S10P22D211-104 (#done)

---
+++ Stickey-frontend/src/App.tsx
@@ -0,0 +1,116 @@
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home/HomePage';
+import SoccerPage from './pages/Home/Soccer/SoccerPage';
+import BaseBallPage from './pages/Home/BaseBall/BaseBallPage';
+import BasketBallPage from './pages/Home/BasketBall/BasketBallPage';
+import AlarmPage from './pages/Alarm/AlarmPage';
+import BookPage from './pages/Book/BookPage';
+import BookNoticePage from './pages/Book/BookNotice/BookNoticePage';
+import BookSectionPage from './pages/Book/BookDetail/BookSectionPage';
+import BookSeatPage from './pages/Book/BookDetail/BookSeatPage';
+import BookPaymentPage from './pages/Book/BookDetail/BookPaymentPage';
+import BookConfirmPage from './pages/Book/BookConfirm/BookConfirmPage';
+import MyTicketPage from './pages/MyTicket/MyTicketPage';
+import SignupPage from './pages/SignUp/SignupPage';
+import LoginPage from './pages/Login/LoginPage';
+import SponsorPage from './pages/Sponsor/SponsorPage';
+import PersonalProfilePage from './pages/Profile/Personal/PersonalProfilePage';
+import PaymentHistoryPage from './pages/Profile/Personal/Payment/PaymentHistory';
+import DreamHistoryPage from './pages/Profile/Personal/Dream/DreamHistoryPage';
+import PreferredTeamPage from './pages/Profile/Personal/PreferredTeam/PreferredTeamPage';
+import ProfileEditPage from './pages/Profile/Personal/ProfileEdit/PersonalProfileEditPage';
+import GroupProfilePage from './pages/Profile/Group/GroupProfilePage';
+import PlayerListPage from './pages/Profile/Group/PlayerList/PlayerListPage';
+
+
+const route = createBrowserRouter([
+  {
+    path:'/',
+    element: <HomePage />
+  },
+  {
+    path:'/soccer',
+    element: <SoccerPage />
+  },
+  {
+    path:'/baseball',
+    element: <BaseBallPage />
+  },
+  {
+    path:'/basketball',
+    element: <BasketBallPage />
+  },
+  {
+    path:'/alarm',
+    element: <AlarmPage />
+  },
+  {
+    path:'/book',
+    element: <BookPage />
+  },
+  {
+    path:'/book/notice',
+    element: <BookNoticePage />
+  },
+  {
+    path:'/book/section',
+    element: <BookSectionPage />
+  },
+  {
+    path:'/book/seat',
+    element: <BookSeatPage />
+  },
+  {
+    path:'/book/payment',
+    element: <BookPaymentPage />
+  },
+  {
+    path:'/book/confirm',
+    element: <BookConfirmPage />
+  },
+  {
+    path:'/mytickets',
+    element: <MyTicketPage />
+  },  
+  {
+    // 이건 컴포넌트로 가져가야할 듯 ?
+    path:'/mytickets/:id',
+    element: <MyTicketPage />
+  },
+  {
+    path:'/mytickets/:id/edit',
+    element: <SignupPage />
+  },
+  {
+    path:'/signup',
+    element: <SignupPage />
+  },
+  {
+    path:'/login',
+    element: <LoginPage />
+  },
+  {
+    path:'/sponsor',
+    element: <SponsorPage />
+  },
+  {
+    path:'/sponsor/:id',
+    element: <SignupPage />
+  },
+  // 단체랑 개인은 role로 구분할 것
+  {
+    
+    path:'/profile',
+    element: <PersonalProfilePage />
+  },
+  {
+    path:'/profile',
+    element: <PersonalProfilePage />
+  },
+    
+]
+
+)
+
+export default route;
