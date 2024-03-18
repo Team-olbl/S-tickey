3e79cde5ceb9f7f61b91741297736463791a141b
강정수 <bbbbqz@nate.com>
2024-03-18 09:46:52 +0900

    feat/S10P22D211-130

--- Stickey-frontend/src/main.tsx
+++ Stickey-frontend/src/main.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -8,12 +7,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
 );
---
+++ Stickey-frontend/src/pages/Profile/ProfilePage.tsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return(
+    <>
+      프로필
+    </>
+  )
+}
+
+export default ProfilePage;
