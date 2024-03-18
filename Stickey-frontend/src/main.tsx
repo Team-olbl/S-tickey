4ffdceaf5861f2f5ed2317f281383d1d7fa1828f
최민경 <kjjpyckjs@gmail.com>
2024-03-18 10:49:21 +0900

    [FE] fix: 오류 수정

--- Stickey-frontend/src/main.tsx
+++ Stickey-frontend/src/main.tsx
@@ -2,13 +2,11 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
 );
