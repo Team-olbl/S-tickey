b014d8ff1c32052eaa4c940e67ed872df49b764c
최부광 <cbgbg0727@naver.com>
2024-03-20 17:41:50 +0900

    [BE] fix: Conflict 중 발생한 오류 수정 #S10P22D211-193

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/user/service/UserServiceImpl.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/user/service/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.olbl.stickeymain.domain.user.service;
 
+import static com.olbl.stickeymain.global.result.error.ErrorCode.EMAIL_VERIFICATION_INVAID;
+import static com.olbl.stickeymain.global.result.error.ErrorCode.EMAIL_VERIFICATION_NOT_EXISTS;
+
+import com.olbl.stickeymain.domain.user.dto.EmailCheckReq;
 import com.olbl.stickeymain.domain.user.dto.EmailCodeReq;
 import com.olbl.stickeymain.domain.user.dto.SignUpReq;
 import com.olbl.stickeymain.domain.user.entity.User;
@@ -10,6 +14,7 @@ import com.olbl.stickeymain.global.util.S3Util;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +27,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final S3Util s3Util;
+    private final RedisTemplate redisTemplate;
 
 
     @Override
@@ -43,6 +49,23 @@ public class UserServiceImpl implements UserService {
 
         // DB 저장
         userRepository.save(user);
+    }
+
+    // 인증 코드 확인
+    @Override
+    public boolean checkAuthEmail(EmailCheckReq emailCheckReq) {
+        // 유효한 인증 코드가 있는지 확인
+        if (!redisTemplate.opsForHash().hasKey("emailAuth", emailCheckReq.getEmail())) {
+            throw new BusinessException(EMAIL_VERIFICATION_NOT_EXISTS);
+        }
+
+        // 인증 코드의 일치 여부 확인
+        String code = (String) redisTemplate.opsForHash()
+            .get("emailAuth", emailCheckReq.getEmail());
+        if (!code.equals(emailCheckReq.getAuthCode())) {
+            throw new BusinessException(EMAIL_VERIFICATION_INVAID);
+        }
+        return true;
     }
 
     @Override
