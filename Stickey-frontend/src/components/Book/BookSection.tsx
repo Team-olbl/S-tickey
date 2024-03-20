be2f6ee078e4ed666a19714a9809effb4607345c
최민경 <kjjpyckjs@naver.com>
2024-03-20 17:09:23 +0900

    [FE] feat: 경기 예매 좌석 선택 ui 구현 #S10P22D211-185

--- Stickey-frontend/src/components/Book/BookSection.tsx
+++ Stickey-frontend/src/components/Book/BookSection.tsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
+import Volley from '../../assets/image/Ground/VolleyballGround.png'
 
 const BookSection = () => {
 
     const [selectedSeat, setSelectedSeat] = useState('');
+
+    const getSeatColor = (seat: string): string => {
+        switch (seat) {
+            case 'S구역 1':
+            case 'S구역 2':
+                return '#FEACAC';
+            case 'R구역 1':
+            case 'R구역 2':
+            case 'R구역 3':
+            case 'R구역 4':
+                return '#D2C2FF';
+            case 'W구역 1':
+            case 'W구역 2':
+                return '#FAF8B7';
+            case 'E구역 1':
+            case 'E구역 2':
+            case 'E구역 3':
+            case 'E구역 4':
+                return '#C3E7FF';
+            default:
+                return '#FFFFFF'; // 기본 색상
+        }
+    };
 
     const handleSeatClick = (seat:string) => {
         setSelectedSeat(seat);
@@ -37,7 +61,7 @@ const BookSection = () => {
                         </div>
                     </div>
                     <div className="flex">
-                        <div className="bg-gray-400 flex h-24 w-40">경기장</div>
+                        <img className="h-24 w-40" src={Volley} />
                     </div>
                     <div className="flex px-1">
                         <div 
@@ -90,7 +114,7 @@ const BookSection = () => {
                 
                 
                 {/* 좌석 구역 정보 */}
-                <div className="pt-4">
+                <div className="py-4">
                     <div className="flex">
                         <div className="flex items-center">
                             <div className="h-2 w-6 bg-[#FEACAC]" />
@@ -116,7 +140,58 @@ const BookSection = () => {
                 </div>
                 
             </div>
-            <div className="text-white">내가 선택한 구역 : {selectedSeat}</div>
+
+            <div className="fixed bottom-0 w-[360px] flex flex-col items-center bg-[#2E2E3D] rounded-t-xl">
+
+            {/* 스텝바 */}
+            <div className="pt-2 w-[150px]">
+
+                <div className="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100">
+                    <ol className="relative z-10 flex justify-between">
+                    <li className="flex items-center">
+                    <span className="size-5 rounded-full bg-gray-100 border-2 border-Stickey_Main text-center text-xs"> 1 </span>
+
+                    </li>
+
+                    <li className="flex items-center p-2">
+                        <span className="size-5 rounded-full bg-gray-100 border-2 border-Stickey_Gray text-center text-xs"> 2 </span>
+                    </li>
+
+                    <li className="flex items-center">
+                        <span className="size-5 rounded-full bg-gray-100 border-2 border-Stickey_Gray text-center text-xs"> 3 </span>
+                    </li>
+                    </ol>
+                </div>
+            </div>
+
+            {/* 사용자 정보 */}
+            <div className="w-full px-16">
+                <div className="items-center grid grid-cols-4 py-3">
+                            <p className="col-span-1 text-xs text-gray-200">좌석등급</p>
+                            <div className="col-span-3 flex items-center">
+                                <div className={`h-2 w-6 mr-2 rounded-md`} style={{ backgroundColor: getSeatColor(selectedSeat) }} />
+                                <div className="text-white text-sm">{selectedSeat}</div>
+                            </div>
+                        </div>
+
+                    <div className="items-center  grid grid-cols-4 py-3">
+                        <p className="col-span-1 text-xs text-gray-200">좌석선택</p>
+                        <div className="col-span-3"></div>
+                    </div>
+
+                    <div className="items-center grid grid-cols-4 py-3">
+                        <p className="col-span-1 text-xs text-gray-200">결제가격</p>
+                        <div className="col-span-3"></div>
+                    </div>
+                </div>
+
+
+                {/* 버튼 */}
+                    <div  className="w-full max-w-[360px] px-4 pt-4 pb-16 flex justify-center">
+                        <button className="bg-Stickey_Gray w-36 mr-2 p-2 text-xs rounded-md">이전</button>
+                        <button className="bg-Stickey_Gray w-36 p-2 text-xs rounded-md">다음</button>
+                    </div>
+            </div>
         </div>
     );
 };
