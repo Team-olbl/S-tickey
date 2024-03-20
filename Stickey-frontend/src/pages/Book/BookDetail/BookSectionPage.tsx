e47ed16a2b9bcd83e872d57622d2598ebb69ce29
최민경 <kjjpyckjs@naver.com>
2024-03-20 13:35:12 +0900

    [FE] feat: 예매 페이지  경기 정보 컴포넌트 ui 구현 #S10P22D211-185

--- Stickey-frontend/src/pages/Book/BookDetail/BookSectionPage.tsx
+++ Stickey-frontend/src/pages/Book/BookDetail/BookSectionPage.tsx
@@ -1,14 +1,35 @@
 import Header, { IHeaderInfo } from "../../../components/@common/Header";
 import NavigationBar from "../../../components/@common/NavigationBar";
 import Back from '../../../assets/image/Back.png'
+import BookInfo from "../../../components/Book/BookInfo";
 
-const BookSectionPage = () => {
+export interface IGameInfo {
+  id: number;
+  stadium: string;
+  homeTeam: string;
+  awayTeam: string;
+  bookStartTime: string;
+  bookEndTime: string;
+  gameStartTime: string;
+}
+
+const BookSectionPage: React.FC<{ gameInfo: IGameInfo }> = () => {
+
+  const dummyGameInfo: IGameInfo = {
+    id: 4,
+    stadium: "DGB대구은행파크",
+    homeTeam: "전북FC",
+    awayTeam: "FC서울",
+    bookStartTime: "2024-03-15T01:42:48",
+    bookEndTime: "2024-03-21T01:42:48",
+    gameStartTime: "2024-03-21T01:42:48"
+  };
 
   const info : IHeaderInfo = {
     left_1:  null,
-    left_2: <img src={Back} alt="" />,
-    center:'',
-    right: <img src="src/assets/Alarm/Bell.png" alt="" />
+    left_2: <img src={Back} />,
+    center:'예매하기',
+    right: <img src="/src/assets/image/Bell.png" />
   }
 
 
@@ -17,10 +38,11 @@ const BookSectionPage = () => {
     <div>
       <Header info={info} />
 
-      <div>
+        <div className="pt-14 pb-16">
+          {/* 경기정보 */}
+          <BookInfo gameInfo={dummyGameInfo} />
+        </div>
 
-
-      </div>
       <NavigationBar />
     </div>
     </>
