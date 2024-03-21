b11a31963885ff74b83a8455fa6f07733caa9713
강정수 <bbbbqz@nate.com>
2024-03-21 16:24:37 +0900

    [FE] feat: 마이페이지-결제내역, 꿈내역 구현 #S10P22D211-164

---
+++ Stickey-frontend/src/components/Profile/Menu.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+const Menu = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-[360px] h-[208px] mt-4 border-t-[0.5px]">
+      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer" onClick={() => navigate('/profile/paymenthistory')}>
+        <p>결제 내역</p>
+        <img src="/src/assets/image/Next.png" className="h-[20px]"/>
+      </div>
+      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer" onClick={() => navigate('/profile/dreamhistory')}>
+        <p>꿈 내역</p>
+        <img src="/src/assets/image/Next.png" className="h-[20px]"/>
+      </div>
+      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer" onClick={() => navigate('/profile/preferredteam')}>
+        <p>나의 선호구단</p>
+        <img src="/src/assets/image/Next.png" className="h-[20px]"/>
+      </div>
+      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer" onClick={() => navigate('/profile/edit')}>
+        <p>회원 정보 수정</p>
+        <img src="/src/assets/image/Next.png" className="h-[20px]"/>
+      </div>
+      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer">
+        <p>로그아웃</p>
+        <img src="/src/assets/image/Next.png" className="h-[20px]"/>
+      </div>
+    </div>
+  )
+}
+
+export default Menu;
