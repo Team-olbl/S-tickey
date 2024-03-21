0ec1da3694ef5ed59141e55d0bda5ec6b6a1c073
윤희원 <gmldnjs427@gmail.com>
2024-03-21 15:51:00 +0900

    [BE] feat: 특정 경기의 전체 구역 별 잔여좌석 조회 api 구현 #S10P22D211-218

--- stickey-main/src/main/java/com/olbl/stickeymain/global/result/error/ErrorCode.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/global/result/error/ErrorCode.java
@@ -20,6 +20,7 @@ public enum ErrorCode {
 
     //Game
     SPORTS_CLUB_DO_NOT_EXISTS(400, "존재하지 않는 스포츠 구단입니다."),
+    GAME_DO_NOT_EXISTS(400, "존재하지 않는 경기입니다."),
     STADIUM_DO_NOT_EXISTS(400, "존재하지 않는 경기장입니다.");
 
     private final int status;
