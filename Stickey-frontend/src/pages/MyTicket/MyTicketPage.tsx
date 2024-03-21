67eba26e47079ceb7cddd0526828cb835faac6ce
최민경 <kjjpyckjs@naver.com>
2024-03-21 20:12:19 +0900

    [FE] fix: 페이지 비율 조정 #S10P22D211-192

--- Stickey-frontend/src/pages/MyTicket/MyTicketPage.tsx
+++ Stickey-frontend/src/pages/MyTicket/MyTicketPage.tsx
@@ -1,8 +1,19 @@
+import Header, { IHeaderInfo } from "../../components/@common/Header";
 import NavigationBar from "../../components/@common/NavigationBar";
+import Bell from '../../assets/image/Bell.png'
 
 const MyTicketPage = () => {
+
+  const info : IHeaderInfo = {
+    left_1:  null,
+    left_2: null,
+    center:'마이티켓',
+    right: <img src={Bell} />
+  }
+  
   return(
     <>
+     <Header info={info} />
       <NavigationBar />
     </>
   )
