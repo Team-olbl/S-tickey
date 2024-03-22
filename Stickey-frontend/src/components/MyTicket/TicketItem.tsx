deebe41350875a941620b271348b42f5a0bdd0f9
최민경 <kjjpyckjs@naver.com>
2024-03-22 17:02:38 +0900

    [FE] feat: 사용전 / 전체 티켓에 따른 정렬 로직 추가 #S10P22D211-183

--- Stickey-frontend/src/components/MyTicket/TicketItem.tsx
+++ Stickey-frontend/src/components/MyTicket/TicketItem.tsx
@@ -1,32 +1,31 @@
-import { ITicket } from "./TicketList";
+import React from 'react';
+import { ITicket } from '../../stores/useTicketSort';
 import Poster from '../../assets/image/Poster.png'
 
-interface TicketItemProps {
-    ticket: ITicket;
+export interface TicketItemProps {
+    ticket: ITicket; 
 }
-
 
 const TicketItem: React.FC<TicketItemProps> = ({ ticket }) => {
-    return(
-        <>
-        <div key={ticket.id}>
+    return (
+        <div className="w-full" key={ticket.id}>
 
             {/* 카드 모양 만들기 */}
-            <div>
-
-                <div className="bg-gray-200 rounded-lg px-2 py-1 text-sm"><p>{ticket.category}{ticket.id}</p></div>
-        
-                <div className="bg-gray-200 p-4 rounded-xl"><img className="" src={Poster} /></div>
-
-                <div className="bg-gray-200 p-2 rounded-lg text-center">
-                    <div className="text-xs">{ticket.gameStartTime}</div>
-                    <div className="font-bold">{ticket.areaId} {ticket.seatNum}</div>
-                    <div className="text-sm">{ticket.stadium}</div>
-                </div>
+            <div className="bg-gray-200 rounded-lg px-2 py-1 text-sm">
+                <p>{ticket.category}{ticket.id}</p>
             </div>
 
+            <div className="bg-gray-200 p-4 rounded-xl">
+                <img src={Poster} alt="Poster"/>
+            </div>
+
+            <div className="bg-gray-200 p-2 rounded-lg text-center">
+                <div className="text-xs">ddddd날짜</div>
+                <div className="font-bold">{ticket.areaId} {ticket.seatNum}</div>
+                <div className="text-sm">{ticket.stadium}</div>
+            </div>
         </div>
-        </>
-    )
-}
+    );
+};
+
 export default TicketItem;
