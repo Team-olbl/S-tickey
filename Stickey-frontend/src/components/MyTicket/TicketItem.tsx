d5a390c6aab7034036c6fb0df03fbf38a9772f0a
최민경 <kjjpyckjs@naver.com>
2024-03-22 16:21:46 +0900

    [FE] feat: 티켓 ui 구현 #S10P22D211-183

--- Stickey-frontend/src/components/MyTicket/TicketItem.tsx
+++ Stickey-frontend/src/components/MyTicket/TicketItem.tsx
@@ -1,4 +1,5 @@
 import { ITicket } from "./TicketList";
+import Poster from '../../assets/image/Poster.png'
 
 interface TicketItemProps {
     ticket: ITicket;
@@ -9,7 +10,21 @@ const TicketItem: React.FC<TicketItemProps> = ({ ticket }) => {
     return(
         <>
         <div key={ticket.id}>
-            <div>{ticket.homeTeam} vs {ticket.awayTeam}</div>
+
+            {/* 카드 모양 만들기 */}
+            <div>
+
+                <div className="bg-gray-200 rounded-lg px-2 py-1 text-sm"><p>{ticket.category}{ticket.id}</p></div>
+        
+                <div className="bg-gray-200 p-4 rounded-xl"><img className="" src={Poster} /></div>
+
+                <div className="bg-gray-200 p-2 rounded-lg text-center">
+                    <div className="text-xs">{ticket.gameStartTime}</div>
+                    <div className="font-bold">{ticket.areaId} {ticket.seatNum}</div>
+                    <div className="text-sm">{ticket.stadium}</div>
+                </div>
+            </div>
+
         </div>
         </>
     )
