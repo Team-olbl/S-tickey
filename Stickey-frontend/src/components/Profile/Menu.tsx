0a497a461490ac69cc0f0653e9d0b4ac33030cc7
강정수 <bbbbqz@nate.com>
2024-03-22 17:01:10 +0900

    [FE] feat: 회원정보수정 UI 및 로그아웃 구현 #S10P22D211-162

--- Stickey-frontend/src/components/Profile/Menu.tsx
+++ Stickey-frontend/src/components/Profile/Menu.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import Next from '../../assets/image/Next.png';
+import { useState } from "react";
+import LogoutModal from "../Login/LogoutModal";
 
 const Menu = () => {
   const navigate = useNavigate();
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState<boolean>(false);
 
   return (
     <div className="max-w-[500px] w-full h-[208px] mt-4 border-t-[0.5px]">
@@ -22,10 +25,11 @@ const Menu = () => {
         <p>회원 정보 수정</p>
         <img src={Next} className="h-[20px]"/>
       </div>
-      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer">
+      <div className="flex flex-row items-center justify-between h-[40px] text-white px-4 cursor-pointer" onClick={() => setIsLogoutModalOpen(true)}>
         <p>로그아웃</p>
         <img src={Next} className="h-[20px]"/>
       </div>
+      {isLogoutModalOpen && <LogoutModal onClose={() => setIsLogoutModalOpen(false)}/>}
     </div>
   )
 }
--- Stickey-frontend/src/pages/Profile/Edit/ProfileEditPage.tsx
+++
@@ -1,9 +0,0 @@
-const ProfileEditPage = () => {
-  return(
-    <>
-      프로필 수정
-    </>
-  )
-}
-
-export default ProfileEditPage;
