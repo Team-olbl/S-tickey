f7b9db4ec5fbe6427f4cc0994207c26f8df64c57
전찬혁 <cksgur5530@gmail.com>
2024-03-24 11:10:08 +0900

    [BC] feat: 결제기록 제외 API #S10P22D211-146

--- stickey-blockchain/contracts/ApplicationHandler.sol
+++ stickey-blockchain/contracts/ApplicationHandler.sol
@@ -11,4 +11,140 @@ contract ApplicationHandler is Api {
     owner = msg.sender;
   }
 
+  
+  /*
+  ========== 경기 API ==========
+  */
+
+  // 게임 정보 설정
+  function setGame(GameInfo memory _game) public isOwner {
+    _setGame(_game);
+  }
+
+  // 경기장 구역 가격 설정
+  function setSeatPrice(uint _stadiumId, uint _zoneId, uint _price) public isOwner {
+    _setSeatPrice(_stadiumId, _zoneId, _price);
+  }
+
+  // 구역 이름 설정
+  function setZoneName(uint _zoneId, string calldata _zoneName) public isOwner {
+    _setZoneName(_zoneId, _zoneName);
+  }
+
+  // 좌석 상태 조회
+  function getSeatState(uint _gameId, uint _zoneId, uint _seatNum) external view returns (bool) {
+    return _getSeatState(_gameId, _zoneId, _seatNum);
+  }
+
+
+  /*
+  ========== 아이템 API ==========
+  */
+
+  // 아이템 조회  
+  function getItemList() external view returns (ItemInfo[] memory, ItemInfo[] memory) {
+    return _getItemList();
+  }
+
+  // 필터 아이템 추가
+  function addFilter(string memory _name, uint _price) public isOwner {
+    _addFilter(_name, _price);
+  }
+  
+  // 필터 아이템 삭제
+  function deleteFilter(uint _id) external isOwner {
+    _deleteFilter(_id);
+  }
+
+  // 배경색 아이템 추가
+  function addBackground(string memory _name, uint _price) public isOwner {
+    _addBackground(_name, _price);
+  }
+  
+  // 배경색 아이템 삭제
+  function deleteBackground(uint _id) external isOwner {
+    _deleteBackground(_id);
+  }
+
+    // 필터 아이템 적용
+  function setFilterOnTicket(uint _tokenId, uint _itemId, uint _supportId) external {
+    _setFilterOnTicket(_tokenId, _itemId, _supportId);
+  }
+
+  // 배경색 아이템 적용
+  function setBackgroundOnTicket(uint _tokenId, uint _itemId, uint _supportId) external {
+    _setBackgroundOnTicket(_tokenId, _itemId, _supportId);
+  }
+
+
+  /*
+  ========== 티켓 API ==========
+  */
+
+  // 티켓 예매
+  function createTicket(uint _number, uint _gameId, uint _stadiumId, uint _zoneId, uint[] calldata _seatNum) external payable {
+    _createTicket(_number, _gameId, _stadiumId, _zoneId, _seatNum);
+  }
+
+  // 티켓 취소 
+  function cancleTicket(uint256 _tokenId) external payable {
+    _cancleTicket(_tokenId);
+  }
+
+  // 가진 티켓 조회
+  function getTickets(address _addr) external view returns (TicketDetail[] memory) {
+    return _getTickets(_addr);
+  }
+
+
+  /*
+  ========== 후원 API ==========
+  */
+
+  // 후원 글 등록
+  function setSupport(uint _id, string calldata _name, address _addr, uint _endTime) external isOwner {
+    _setSupport(_id, _name, _addr, _endTime);
+  }
+
+  // 후원
+  function donate(uint _supportId, string memory _text) external payable {
+    _donate(_supportId, msg.value, _text);
+  }
+
+  // 후원금 수령
+  function withdraw(uint _supportId) external {
+    _withdraw(_supportId);
+  }
+
+  // 후원받은 내역 조회
+  function getSupprtedHistory(uint _supportId) external view returns(SupportedHistory[] memory) {
+    return _getSupptedHistory(_supportId);
+  }
+
+  // 후원한 내역 조회
+  function getSupprtingHistory(address _addr) external view returns(SupportingHistory[] memory) {
+    return _getSupportingHistory(_addr);
+  }
+
+
+  /*
+  ========== 꿈 API ==========
+  */
+
+  // 꿈 증감 내역 조회
+  function getRewordHistory(address _addr) external view returns (Reword.RewordHistory[] memory) {
+    return reword.getRewordHistory(_addr); 
+  }
+
+  // 꿈 잔액 조회
+  function getReword(address _addr) external view returns (uint) {
+    return reword.balanceOf(_addr);
+  }
+
+
+
+  modifier isOwner() {
+    require(msg.sender == owner, "Permission denied.");
+    _;
+  }
 }
