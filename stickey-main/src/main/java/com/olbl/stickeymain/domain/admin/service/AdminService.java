9502ba8a48caba89cac3d0d90fcd341f28e83011
윤희원 <gmldnjs427@naver.com>
2024-03-24 21:31:36 +0900

    [BE] feat: 승인 대기 단체 회원 상세 조회 api 구현 #S10P22D211-196

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/admin/service/AdminService.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/admin/service/AdminService.java
@@ -1,8 +1,11 @@
 package com.olbl.stickeymain.domain.admin.service;
 
 import com.olbl.stickeymain.domain.admin.dto.SignUpListRes;
+import com.olbl.stickeymain.domain.admin.dto.SignUpOneRes;
 
 public interface AdminService {
 
     SignUpListRes getSignUpList();
+
+    SignUpOneRes getSignUp(int id);
 }
--- stickey-main/src/main/java/com/olbl/stickeymain/domain/admin/service/AdminServiceImpl.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/admin/service/AdminServiceImpl.java
@@ -1,9 +1,15 @@
 package com.olbl.stickeymain.domain.admin.service;
 
+import static com.olbl.stickeymain.global.result.error.ErrorCode.ORGANIZATION_SIGNUP_DO_NOT_EXISTS;
+import static com.olbl.stickeymain.global.result.error.ErrorCode.ORGANIZATION_SIGNUP_NOT_WAITING;
+
 import com.olbl.stickeymain.domain.admin.dto.SignUpListRes;
+import com.olbl.stickeymain.domain.admin.dto.SignUpOneRes;
 import com.olbl.stickeymain.domain.admin.dto.SignUpRes;
+import com.olbl.stickeymain.domain.user.organization.entity.Organization;
 import com.olbl.stickeymain.domain.user.organization.entity.OrganizationStatus;
 import com.olbl.stickeymain.domain.user.organization.repository.OrganizationRepository;
+import com.olbl.stickeymain.global.result.error.exception.BusinessException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,5 +28,28 @@ public class AdminServiceImpl implements AdminService {
         List<SignUpRes> signUpResList = organizationRepository.findAllByStatus(
             OrganizationStatus.WAITING);
         return new SignUpListRes(signUpResList, signUpResList.size());
+    }
+
+    @Override
+    public SignUpOneRes getSignUp(int id) {
+        //TODO: 관리자 계정 확인 로직
+        Organization organization = organizationRepository.findById(id)
+            .orElseThrow(() -> new BusinessException(ORGANIZATION_SIGNUP_DO_NOT_EXISTS));
+        if (organization.getStatus() != OrganizationStatus.WAITING) {
+            throw new BusinessException(ORGANIZATION_SIGNUP_NOT_WAITING);
+        }
+
+        SignUpOneRes signUpOneRes = new SignUpOneRes();
+
+        signUpOneRes.setName(organization.getName());
+        signUpOneRes.setPhone(organization.getPhone());
+        signUpOneRes.setEmail(organization.getEmail());
+        signUpOneRes.setProfileImage(organization.getProfileImage());
+        signUpOneRes.setManager(organization.getManager());
+        signUpOneRes.setAddress(organization.getAddress());
+        signUpOneRes.setRegistrationNumber(organization.getRegistrationNumber());
+        signUpOneRes.setRegistrationFile(organization.getRegistrationFile());
+
+        return signUpOneRes;
     }
 }
