cc384417c973a14544f7c5070d7e29fbd6af27de
윤희원 <gmldnjs427@naver.com>
2024-03-24 22:31:26 +0900

    [BE] feat: 단체 회원 승인 및 거절 api 구현 #S10P22D211-198

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/admin/service/AdminServiceImpl.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/admin/service/AdminServiceImpl.java
@@ -1,17 +1,26 @@
 package com.olbl.stickeymain.domain.admin.service;
 
+import static com.olbl.stickeymain.domain.user.organization.entity.OrganizationStatus.ACCEPTED;
+import static com.olbl.stickeymain.domain.user.organization.entity.OrganizationStatus.REJECTED;
+import static com.olbl.stickeymain.global.result.error.ErrorCode.ORGANIZATION_SIGNUP_DO_NOT_EXISTS;
+import static com.olbl.stickeymain.global.result.error.ErrorCode.ORGANIZATION_SIGNUP_NOT_WAITING;
+
 import com.olbl.stickeymain.domain.admin.dto.SignUpListRes;
 import com.olbl.stickeymain.domain.admin.dto.SignUpRes;
+import com.olbl.stickeymain.domain.user.organization.entity.Organization;
 import com.olbl.stickeymain.domain.user.organization.entity.OrganizationStatus;
 import com.olbl.stickeymain.domain.user.organization.repository.OrganizationRepository;
+import com.olbl.stickeymain.global.result.error.exception.BusinessException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class AdminServiceImpl implements AdminService {
 
     private final OrganizationRepository organizationRepository;
@@ -22,5 +31,24 @@ public class AdminServiceImpl implements AdminService {
         List<SignUpRes> signUpResList = organizationRepository.findAllByStatus(
             OrganizationStatus.WAITING);
         return new SignUpListRes(signUpResList, signUpResList.size());
+    }
+
+    @Override
+    @Transactional
+    public void confirmOrganization(int id, String status) {
+        //TODO: 관리자 계정 확인 로직
+        Organization organization = organizationRepository.findById(id)
+            .orElseThrow(() -> new BusinessException(ORGANIZATION_SIGNUP_DO_NOT_EXISTS));
+        if (organization.getStatus() != OrganizationStatus.WAITING) {
+            throw new BusinessException(ORGANIZATION_SIGNUP_NOT_WAITING);
+        }
+
+        if (status.equals("1")) {
+            organization.setStatus(ACCEPTED);
+        } else if (status.equals("2")) {
+            organization.setStatus(REJECTED);
+        }
+
+        organizationRepository.save(organization);
     }
 }
