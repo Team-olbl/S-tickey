1e6dcfd9ff07fac4071aa8ed1c65485537458cbf
전찬혁 <cksgur5530@gmail.com>
2024-03-26 01:36:51 +0900

    [FE] feat: 메타마스크 지갑 연동 및 지갑 정보 조회 #S10P22D211-240

--- Stickey-frontend/src/BlockchainTest.tsx
+++ Stickey-frontend/src/BlockchainTest.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
+import { connect, getWalletInfo } from './service/ethers/api'
+
 const BlockchainTest = () => {
 
+  useEffect(() => {
+    connect();
+  }, []);
 
+  const _connect = () => {
+    connect();
+  }
+
+  const _getWalletInfo = async () => {
+    const result = await getWalletInfo();
+    console.log(result);
+  }
 
   return <>
     <div className="text-red-500">
       <div>블록체인 테스트 화면입니다.</div>
- 
-      
+      <div onClick={_connect}>메타마스크 지갑연결  버튼</div>
+      <div onClick={_getWalletInfo}>지갑 정보 확인</div>
     </div>
   </>
 
