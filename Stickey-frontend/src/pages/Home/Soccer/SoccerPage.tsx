ad25f6c6f8a1337af6653f97373c868c823dc472
최민경 <kjjpyckjs@gmail.com>
2024-03-25 15:43:31 +0900

    [FE] fix: 오류 수정

--- Stickey-frontend/src/pages/Home/Soccer/SoccerPage.tsx
+++ Stickey-frontend/src/pages/Home/Soccer/SoccerPage.tsx
@@ -7,8 +7,21 @@ import BottomSheet from "../../../components/@common/BottomSheet";
 import TeamList from "../../../components/Soccer/TeamList";
 import Calendar from "../../../components/@common/Calendar";
 import Hushed from "../../../assets/image/MatchItem.png";
-import MatchItem from "../../../components/Soccer/MatchItem";
+// import MatchItem from "../../../components/Soccer/MatchItem";
 import { useGame } from "../../../hooks/Home/useGame";
+
+export type MatchItemData = {
+  id: number;
+  stadium: string;
+  homeTeam: string;
+  homeTeamLogo: JSX.Element;
+  awayTeam: string;
+  awayTeamLogo: JSX.Element;
+  bookStartTime: string;
+  bookEndTime: string;
+  gameStartTime: string;
+}
+
 
 const SoccerPage = () => {
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState<boolean>(false);
@@ -17,6 +30,22 @@ const SoccerPage = () => {
   const [selectedMonth, setSelectedMonth] = useState<number>(today.getMonth());
   const [selectedYear, setSelectedYear] = useState<number>(today.getFullYear());
 
+  const dummies:MatchItemData[] = [
+    {
+      id: 1,
+      stadium: "DGB대구은행파크",
+      homeTeam: "대구FC",
+      homeTeamLogo: <img />,
+      awayTeam: "광주FC",
+      awayTeamLogo: <img />,
+      bookStartTime: "2024-03-15T01:42:48",
+      bookEndTime: "2024-03-21T01:42:48",
+      gameStartTime: "2024-03-21T01:42:48"
+    },
+
+  ]
+  console.log(dummies)
+  
   const openBottomSheet = () => {
     setIsBottomSheetOpen(true);
   }
@@ -51,14 +80,14 @@ const SoccerPage = () => {
     right: <img src={Bell} alt="" />
   }
 
-  const filteredMatches = gameListInfo?.data ? gameListInfo.data.gameResList.filter((data) => {
+  const filteredMatches =  gameListInfo?.data.gameResList.filter((data) => {
     const matchDate = new Date(data.gameStartTime);
     return (
       matchDate.getDate() === selectedDate &&
       matchDate.getMonth() === selectedMonth &&
       matchDate.getFullYear() === selectedYear
     );
-  }) : [];
+  });
 
   return(
     <>
@@ -75,7 +104,7 @@ const SoccerPage = () => {
         ) : (
           filteredMatches?.map((item) =>(
             <div className="" key={item.id}>
-              <MatchItem data={item} />
+              {/* <MatchItem data={item} /> */}
             </div>
           ))
         )}
