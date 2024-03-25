55b30b2e8213bf180a469edea1ac6042321a203d
jiyun3016 <jiyun3016@gmail.com>
2024-03-26 06:41:49 +0900

    [BE] feat: 후원 글 상세조회 api #S10P22D211-235

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/support/repository/SupportRepository.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/support/repository/SupportRepository.java
@@ -5,8 +5,8 @@ import com.olbl.stickeymain.domain.admin.dto.WaitingSupportOneRes;
 import com.olbl.stickeymain.domain.admin.dto.WaitingSupportRes;
 import com.olbl.stickeymain.domain.support.entity.Support;
 import com.olbl.stickeymain.domain.support.entity.SupportStatus;
-import com.olbl.stickeymain.domain.user.dto.MySupportRes;
 import com.olbl.stickeymain.domain.user.dto.MySupportOneRes;
+import com.olbl.stickeymain.domain.user.dto.MySupportRes;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
@@ -23,5 +23,5 @@ public interface SupportRepository extends JpaRepository<Support, Integer>,
     Slice<MySupportRes> findAllByOrganizationId(int id, Pageable pageable);
 
     Optional<MySupportOneRes> findMySupportOneById(int id);
-
+    
 }
--- stickey-main/src/main/java/com/olbl/stickeymain/domain/support/service/SupportService.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/support/service/SupportService.java
@@ -1,6 +1,7 @@
 package com.olbl.stickeymain.domain.support.service;
 
 import com.olbl.stickeymain.domain.support.dto.SupportListRes;
+import com.olbl.stickeymain.domain.support.dto.SupportOneRes;
 import com.olbl.stickeymain.domain.support.dto.SupportReq;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,4 +10,6 @@ public interface SupportService {
     void registSupport(SupportReq supportReq, MultipartFile supportImage);
 
     SupportListRes getSupportList(Integer flag);
+
+    SupportOneRes getSupportOneById(int supportId);
 }
