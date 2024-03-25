8801652261e731ce6778c3ac99e22a09b7b069b2
전찬혁 <cksgur5530@gmail.com>
2024-03-26 02:16:32 +0900

    [FE] feat: createTicket #S10P22D211-241

--- Stickey-frontend/src/service/ethers/api.ts
+++ Stickey-frontend/src/service/ethers/api.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Web3 from 'web3';
 import { contractABI } from './Abi';
-
 const contractAddress = import.meta.env.VITE_APP_CONTRACT_ADDRESS;
 
 let web3 : any = null;
@@ -19,6 +18,8 @@ const requestAccount = async () => {
   console.log("account", account);
 }
 
+// 지갑 연결
+// 처음 연결할때 사용
 export const connect = async () => {
   if (!window.ethereum) {
     alert("메타마스크를 설치하세요.");
@@ -34,6 +35,11 @@ export const connect = async () => {
   }
 } 
 
+/** 
+ * 지갑 정보 확인 API
+ * 
+ * @return { address: 지갑주소, balance : 지갑 내 이더리움 잔액, dream : 꿈 잔액 }
+ */
 export const getWalletInfo = async () => {
   if (contract === null || web3 === null) throw new Error("Invalid Call");
   const balance = await web3?.eth.getBalance(account[0]);
@@ -42,4 +48,22 @@ export const getWalletInfo = async () => {
 
   const data = { address: account[0], balance : etherValue, dream : dream.toString()}
   return data;
+}
+
+
+/**
+ * 티켓 예매 API
+ * 
+ * @param number 티켓 매수 (좌석 선택 수)
+ * @param gameId 경기 ID
+ * @param stadium 경기장 이름
+ * @param zoneName 구역 이름
+ * @param seatNumber 선택한 좌석번호들
+ * @return Transaction Hash 
+ */
+export const createTicket = async (number : number, gameId : number, stadium : stirng, zoneName : string, seatNumber : number[]) => {
+  if (contract === null || web3 === null) throw new Error("Invalid Call");
+  const ret = await contract.methods.createTicket(number, gameId, stadium, zoneName, seatNumber).send({ from: account[0] });
+
+  return ret;
 }
