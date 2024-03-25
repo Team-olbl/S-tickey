d659448091878b7852da0407d59818fdb04f9d0b
전찬혁 <cksgur5530@gmail.com>
2024-03-26 02:16:32 +0900

    [FE] feat: createTicket #S10P22D211-241

--- Stickey-frontend/src/BlockchainTest.tsx
+++ Stickey-frontend/src/BlockchainTest.tsx
@@ -17,10 +17,20 @@ const BlockchainTest = () => {
   }
 
   return <>
-    <div className="text-red-500">
+    <div className="text-slate-300 flex flex-col items-center">
       <div>블록체인 테스트 화면입니다.</div>
-      <div onClick={_connect}>메타마스크 지갑연결  버튼</div>
-      <div onClick={_getWalletInfo}>지갑 정보 확인</div>
+
+      <div className="text-center bg-amber-600">
+        <div className="text-[5vw]">지갑 관련 API</div>
+        <div className="flex gap-5">
+        <button type="button" onClick={_connect} >
+          메타마스크 지갑연결 
+          </button>
+          <button type="button" onClick={_getWalletInfo} >
+          지갑 정보 확인
+          </button>
+        </div>
+      </div>
     </div>
   </>
 
