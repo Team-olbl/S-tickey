59ed3af45d92527c5432bdffc9082e8a842b794d
전찬혁 <cksgur5530@gmail.com>
2024-03-26 14:24:21 +0900

    [FE] feat: getTickets, refundTicket #S10P22D211-241

--- Stickey-frontend/src/BlockchainTest.tsx
+++ Stickey-frontend/src/BlockchainTest.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
-import { connect, getWalletInfo } from './service/ethers/api'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef } from "react";
+import { connect, createTicket, getTickets, getWalletInfo, refundTicket } from './service/web3/api'
 
 const BlockchainTest = () => {
+
+  const refundRet = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     connect();
@@ -14,14 +17,37 @@ const BlockchainTest = () => {
   const _getWalletInfo = async () => {
     const result = await getWalletInfo();
     console.log(result);
+    alert("실행완료");
+  }
+
+  const _createTicket = async (event : any) => {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+
+    const data = Object.fromEntries(fd.entries());
+    const seatNumber : any = fd.getAll('seatNumber');
+    console.log(data, seatNumber);
+
+    const result = await createTicket(Number(data.number), Number(data.gameId), Number(data.stadiumId), Number(data.zoneId), seatNumber);
+    console.log(result);
+    alert("실행완료");
+  } 
+  
+  const _getTickets = async () => {
+    const result = await getTickets();
+    console.log(result);
+    alert("실행완료");
+  }
+
+  const _refundTicket = async () => {
+    refundTicket(Number(refundRet.current!.value));
   }
 
   return <>
-    <div className="text-slate-300 flex flex-col items-center">
-      <div>블록체인 테스트 화면입니다.</div>
+    <div className="text-black flex flex-col items-center w-full">
 
-      <div className="text-center bg-amber-600">
-        <div className="text-[5vw]">지갑 관련 API</div>
+      <div className="text-center bg-amber-600 w-full">
+        <div className="text-[20px] underline underline-offset-2">지갑 관련 API</div>
         <div className="flex gap-5">
         <button type="button" onClick={_connect} >
           메타마스크 지갑연결 
@@ -29,6 +55,43 @@ const BlockchainTest = () => {
           <button type="button" onClick={_getWalletInfo} >
           지갑 정보 확인
           </button>
+        </div>
+      </div>
+
+      <div className="text-center bg-green-400 w-full">
+        <div className="text-[20px] underline underline-offset-2">예매 관련 API</div>
+        <div className="flex gap-5 w-full">  
+          <div className="flex-row items-center justify-center w-full">
+            <div className="w-full">
+              <div >
+                <form action="" onSubmit={_createTicket} className="flex flex-col gap-1 w-full items-center">
+                  티켓 매수<input type="text" name="number" />
+                  경기ID<input type="text" name="gameId"/>
+                  경기장 ID<input type="text" name="stadiumId" />
+                  구역 ID<input type="text" name="zoneId"/>
+                  좌석 번호 
+                    <select name="seatNumber" id="" size={2} multiple>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                  </select>
+                  <button>예매하기</button>
+                  </form>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button onClick={_getTickets}>내 티켓 확인</button>
+          </div>
+          
+          <div className="w-[20%]">
+            <div>티켓 환불</div>
+            티켓ID<input type="text" ref={refundRet} />
+            <button onClick={_refundTicket}>환불하기</button>
+          </div>
+
+
         </div>
       </div>
     </div>
---
+++ Stickey-frontend/src/service/web3/api.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Web3 from 'web3';
+import { contractABI } from './Abi';
+const contractAddress = "0x7C20b2c35Fb7aaF6f59166Aa50e3951db72a813b";
+
+let web3 : any = null;
+let contract: any = null;
+let account : any = [] ;
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+// 지갑 연결
+// 처음 연결할때 사용
+export const connect = async () => {
+  if (!window.ethereum) {
+    alert("메타마스크를 설치하세요.");
+    return;
+  } 
+  
+  web3 = new Web3(window.ethereum);
+  try {
+    await requestAccount();
+    contract = new web3.eth.Contract(contractABI, contractAddress);
+  } catch (err) {
+    console.log(err);
+  }
+} 
+
+const requestAccount = async () => {
+  account = await window.ethereum?.request({ method: 'eth_requestAccounts' });
+  web3.eth.handleRevert = true
+}
+
+export const getWalletInfo = async () => {
+  if (contract === null || web3 === null) throw new Error("Invalid Call");
+  const balance = await web3?.eth.getBalance(account[0]);
+  const dream = await contract.methods.getReword(account[0]).call();
+  const etherValue = web3?.utils.fromWei(balance, 'ether');
+
+  const data = { address: account[0], balance : etherValue, dream : dream.toString()}
+  return data;
+}
+
+export const createTicket = async (number : number, gameId : number, stadiumId : number, zoneId : number, seatNumber : number[]) => {
+  if (contract === null || web3 === null) throw new Error("Invalid Call");
+  const value = 10 ** 9 * number;
+  const ret = await contract.methods.createTicket(number, gameId, stadiumId, zoneId, seatNumber).send({ from: account[0], value : value });
+
+  return ret;
+}
+
+export const refundTicket = async (tokenId: number) => {
+  if (contract === null || web3 === null) throw new Error("Invalid Call");
+  try {
+    const ret = await contract.methods.refundTicket(tokenId).send({ from: account[0] });
+    return ret;
+  } catch (error: any) {
+    console.log(error);
+    const errObj = JSON.parse(error.message.split("\n")[1]);
+    console.log(errObj);
+  }
+}
+
+export const getTickets = async () => {
+  if (contract === null || web3 === null) throw new Error("Invalid Call");
+  const ret = await contract.methods.getTickets(account[0]).call();
+
+  const data = [];
+
+  for (let i = ret.length-1; i >= 0; i--) {
+    data.push(
+      {
+        tokenId: ret[i].tokenId,
+        gameId: ret[i].gameId,
+        stadium: ret[i].stadium,
+        seat: ret[i].zoneName + " " + ret[i].seatNumber,
+        price: ret[i].price,
+        filterId: ret[i].filterId,
+        backgroundId: ret[i].backgroundId,
+        category: ret[i].category == 0 ? "SOCCER" : ret[i].category == 1 ? "BASEBALL" : "BESKETBALL",
+        homeTeam: ret[i].homeTeam,
+        awayTeam: ret[i].awayTeam,
+        gameImage: ret[i].gameImage
+      }
+    )
+  }
+  return data;
+}
+
+
+
