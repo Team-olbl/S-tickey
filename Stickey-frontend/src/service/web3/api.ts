a6c9c1f5c592b581b7d97a9e0b3b5d9fbada4095
전찬혁 <cksgur5530@gmail.com>
2024-03-26 20:07:01 +0900

    [FE] feat: 티켓 블록체인 API #S10P22D211-241

--- Stickey-frontend/src/service/web3/api.ts
+++ Stickey-frontend/src/service/web3/api.ts
@@ -36,20 +36,27 @@ const requestAccount = async () => {
 
 export const getWalletInfo = async () => {
   if (contract === null || web3 === null) throw new Error("Invalid Call");
-  const balance = await web3?.eth.getBalance(account[0]);
-  const dream = await contract.methods.getReword(account[0]).call();
-  const etherValue = web3?.utils.fromWei(balance, 'ether');
+  try {
+    const balance = await web3?.eth.getBalance(account[0]);
+    const dream = await contract.methods.getReword(account[0]).call();
+    const etherValue = web3?.utils.fromWei(balance, 'ether');
 
-  const data = { address: account[0], balance : etherValue, dream : dream.toString()}
-  return data;
+    const data = { address: account[0], balance: etherValue, dream: dream.toString() }
+    return data;
+  } catch (err) {
+    alert("지갑 조회 실패");
+  }
 }
 
 export const createTicket = async (number : number, gameId : number, stadiumId : number, zoneId : number, seatNumber : number[]) => {
   if (contract === null || web3 === null) throw new Error("Invalid Call");
   const value = 10 ** 9 * number;
-  const ret = await contract.methods.createTicket(number, gameId, stadiumId, zoneId, seatNumber).send({ from: account[0], value : value });
-
-  return ret;
+  try {
+    const ret = await contract.methods.createTicket(number, gameId, stadiumId, zoneId, seatNumber).send({ from: account[0], value: value });
+    return ret;
+  } catch (err) {
+    alert("티켓 예매 실패");
+  }
 }
 
 export const refundTicket = async (tokenId: number) => {
@@ -57,37 +64,39 @@ export const refundTicket = async (tokenId: number) => {
   try {
     const ret = await contract.methods.refundTicket(tokenId).send({ from: account[0] });
     return ret;
-  } catch (error: any) {
-    console.log(error);
-    const errObj = JSON.parse(error.message.split("\n")[1]);
-    console.log(errObj);
+  } catch (err) {
+    alert("환불 실패");
   }
 }
 
 export const getTickets = async () => {
   if (contract === null || web3 === null) throw new Error("Invalid Call");
-  const ret = await contract.methods.getTickets(account[0]).call();
+  try {
+    const ret = await contract.methods.getTickets(account[0]).call();
 
-  const data = [];
+    const data = [];
 
-  for (let i = ret.length-1; i >= 0; i--) {
-    data.push(
-      {
-        tokenId: ret[i].tokenId,
-        gameId: ret[i].gameId,
-        stadium: ret[i].stadium,
-        seat: ret[i].zoneName + " " + ret[i].seatNumber,
-        price: ret[i].price,
-        filterId: ret[i].filterId,
-        backgroundId: ret[i].backgroundId,
-        category: ret[i].category == 0 ? "SOCCER" : ret[i].category == 1 ? "BASEBALL" : "BESKETBALL",
-        homeTeam: ret[i].homeTeam,
-        awayTeam: ret[i].awayTeam,
-        gameImage: ret[i].gameImage
-      }
-    )
+    for (let i = ret.length - 1; i >= 0; i--) {
+      data.push(
+        {
+          tokenId: ret[i].tokenId,
+          gameId: ret[i].gameId,
+          stadium: ret[i].stadium,
+          seat: ret[i].zoneName + " " + ret[i].seatNumber,
+          price: ret[i].price,
+          filterId: ret[i].filterId,
+          backgroundId: ret[i].backgroundId,
+          category: ret[i].category == 0 ? "SOCCER" : ret[i].category == 1 ? "BASEBALL" : "BESKETBALL",
+          homeTeam: ret[i].homeTeam,
+          awayTeam: ret[i].awayTeam,
+          gameImage: ret[i].gameImage
+        }
+      )
+    }
+    return data;
+  } catch (err) {
+    alert("티켓 조회 실패");
   }
-  return data;
 }
 
 
