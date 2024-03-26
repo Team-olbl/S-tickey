445bdef41dc8fcb8b4cc11e2e2e2e4db01d1aae7
최민경 <kjjpyckjs@naver.com>
2024-03-27 00:14:21 +0900

    [FE] feat: 단체 마이페이지 후원글 조회 예외처리 및 페이지 구현 #S10P22D211-266

--- Stickey-frontend/src/pages/Profile/Group/SponListPage.tsx
+++ Stickey-frontend/src/pages/Profile/Group/SponListPage.tsx
@@ -5,16 +5,6 @@ import Bell from '../../../assets/image/Bell.png';
 import NavigationBar from "../../../components/@common/NavigationBar";
 import SponItem from "../../../components/Profile/Group/SponItem";
 
-export type SponData = {
-  id: number;
-  title: string;
-  start_date: string;
-  end_date: string;
-  content: string;
-  support_img: JSX.Element;
-  organization_profile: JSX.Element;
-}
-
 const SponListPage = () => {
   const navigate = useNavigate();
 
@@ -29,35 +19,13 @@ const SponListPage = () => {
     right: <img src={Bell} />
   }
 
-  const dummies: SponData[] = [
-    {
-      id: 1,
-      title: 'D211 팀원들에게 사랑을 주세요',
-      start_date: "2024-03-00",
-      end_date: "2024-03-15",
-      content: '후원받은 금액으로 맛있는 간식을 먹을 예정입니다.',
-      support_img: <img src="" />,
-      organization_profile: <img src="" />
-    },
-    {
-      id: 2,
-      title: 'D211 팀원들에게 사랑을 주세요',
-      start_date: "2024-03-16",
-      end_date: "2024-03-31",
-      content: '후원받은 금액으로 맛있는 간식을 먹을 예정입니다.',
-      support_img: <img src="" />,
-      organization_profile: <img src="" />
-    },
-  ];
-
   return(
     <>
       <Header info={info}/>
       <div className="pt-12">
-        {/* 단체 회원일 경우에만 보이도록 로직 추가해야함 */}
-        {dummies.map((item) => (
-          <SponItem  key={item.id} data={item} />
-        ))}
+          <SponItem />
+
+        {/* 후원글 등록 버튼 */}
         <div className="fixed bottom-16 w-full max-w-[500px] px-4 flex justify-end">
             <button className="flex justify-center text-4xl text-white bg-Stickey_Main w-12 h-12 rounded-full" onClick={() => gotoCreatePage()}>
             +</button>
