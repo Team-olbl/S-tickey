9426effd2472923093e94fc60a7e592df9af65c8
최민경 <kjjpyckjs@naver.com>
2024-03-26 22:55:45 +0900

    [FE] feat: 단체 선수 리스트 조회 api 연결 및 예외처리 구현 #S10P22D211-265

---
+++ Stickey-frontend/src/components/Profile/Group/PlayerList.tsx
@@ -0,0 +1,62 @@
+// import { useProfile } from "../../../hooks/Profile/useProfile";
+import { IPlayerListRes } from "../../../types/Profile";
+import Player from '../../../assets/image/Player.png'
+
+const PlayerList = () => {
+  
+  // TODO 유저 완성 후 실제 api 데이터로 수정해야함
+  // const { useGetPlayerList } = useProfile();
+  // const { data: playerListInfo } = useGetPlayerList();
+  // const playerList = playerListInfo?.data.playerResList || [];
+
+  const dummyPlayerList: IPlayerListRes = {
+    playerResList: []
+  }
+
+  return (
+    <div>
+    <div className="w-full flex flex-col justify-center gap-3 items-center px-4">
+      {dummyPlayerList.playerResList.length > 0 ? (
+        dummyPlayerList.playerResList.map((item, id) => (
+          <div key={id} className="px-4">
+            <div className="flex items-center h-auto py-4 px-2 border-none rounded-[10px] bg-[#2E2E3D]">
+              <div className="w-full flex flex-row px-4 gap-3">
+                <div className="w-20 h-20 border border-none rounded-full bg-Stickey_Gray">프로필 사진</div>
+                <div className="flex flex-col w-[208px] h-[20px] gap-2 text-white">
+                  <div className="flex flex-row gap-1">
+                    <div className="flex flex-col justify-end">
+                      <p className="text-[8px]">{item.category}</p>
+                    </div>
+                    <p className="text-[12px]">{item.name}</p>
+                  </div>
+                  <div className="border-b-[0.5px] w-full"></div>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex flex-row">
+                      <p className="w-12 h-[10px] text-[8px] text-[#A0A0A0]">생년월일</p>
+                      <p className="w-12 h-[10px] text-[8px]">{item.birth}</p>
+                    </div>
+                    <div className="flex flex-row">
+                      <p className="w-12 h-10 text-[8px] text-[#A0A0A0]">선수 설명</p>
+                      <div className="flex flex-col gap-1">{item.description}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="flex flex-col items-center pt-40">
+          <img className="w-32" src={Player} />
+          <div className="text-white text-center text-sm py-4">
+            <p className="py-4">아직 등록된 유망주 선수가 없어요 !</p>
+            <p>후원 유망주를 등록해주세요.</p>
+          </div>
+        </div>
+      )}
+    </div>
+  </div>
+  );
+};
+
+export default PlayerList;
