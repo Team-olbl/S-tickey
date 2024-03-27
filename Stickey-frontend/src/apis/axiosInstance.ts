052b95e6aeffe93ff01f0c7988c877b10e810dc2
강정수 <bbbbqz@nate.com>
2024-03-27 12:36:48 +0900

    [FE] feat: 이메일 인증코드 발송 api #S10P22D211-262

--- Stickey-frontend/src/apis/axiosInstance.ts
+++ Stickey-frontend/src/apis/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import setAuthorization from './imterceptors';
+import setAuthorization from './interceptors';
 
 const axiosRequestConfig = {
   baseURL: import.meta.env.VITE_BASE_URL,
