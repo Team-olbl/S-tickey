3c328860c3bde1c9d7c2d9aa1b7757270a42b99e
강정수 <bbbbqz@nate.com>
2024-03-27 16:42:49 +0900

    [FE] feat: 인증번호 검증 구현 #S10P22D211-262

--- Stickey-frontend/src/service/User/api.ts
+++ Stickey-frontend/src/service/User/api.ts
@@ -1,10 +1,18 @@
 import { axiosCommonInstance } from "../../apis/axiosInstance";
+import { confirmEmailReq, confirmEmailRes } from "../../types/Email";
 
 export const sendEmailVerification = async (email:string) => {
   const {data} = await axiosCommonInstance.post(`users/auth`, {
     "email" : email
   },
   );
-  // console.log({email})
+  return data;
+}
+
+export const confirmEmailVerification = async ({email, authCode}: confirmEmailReq):Promise<confirmEmailRes> => {
+  const {data} = await axiosCommonInstance.post(`users/auth-check`, {
+    "email" : email,
+    "authCode" : authCode,
+  })
   return data;
 }
