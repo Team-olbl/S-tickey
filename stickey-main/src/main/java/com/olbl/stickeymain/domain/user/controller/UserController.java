006b5d82445e67c9e04fb9165a5619cb26814907
윤희원 <gmldnjs427@naver.com>
2024-03-27 14:35:07 +0900

    [BE] fix: 프로필 조회 시 기관 상태값, role 반환하게 수정 #S10P22D211-287

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/user/controller/UserController.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/user/controller/UserController.java
@@ -29,9 +29,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -96,9 +96,9 @@ public class UserController {
     }
 
     @Operation(summary = "회원 프로필 조회 (본인이)")
-    @GetMapping("/profiles/{id}")
-    public ResponseEntity<ResultResponse> getProfile(@PathVariable(value = "id") int id) {
-        ProfileRes profile = userService.getProfile(id);
+    @GetMapping("/profiles")
+    public ResponseEntity<ResultResponse> getProfile(Authentication authentication) {
+        ProfileRes profile = userService.getProfile(authentication);
         return ResponseEntity.ok(ResultResponse.of(GET_PROFILE_SUCCESS, profile));
     }
 
