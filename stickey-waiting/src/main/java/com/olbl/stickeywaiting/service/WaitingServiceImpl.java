735c49f1675cdedd4fafe8be4e7724efe3f3582b
최부광 <cbgbg0727@knu.ac.kr>
2024-03-27 17:56:47 +0900

    [BE] feat: 대기열 스케줄러 구현 #S10P22D211-280

--- stickey-waiting/src/main/java/com/olbl/stickeywaiting/service/WaitingServiceImpl.java
+++ stickey-waiting/src/main/java/com/olbl/stickeywaiting/service/WaitingServiceImpl.java
@@ -18,14 +18,15 @@ public class WaitingServiceImpl implements WaitingService {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final SimpMessagingTemplate template;
-    private final int popAmount = 1; // 한 번에 참가열로 이동 시킬 사람의 수
-    private final String waitKey = "wait::";
+    private final int popAmount = 3; // 한 번에 작업열로 이동 시킬 사람의 수
+    private final String baseWaitKey = "wait::";
+    private final String baseRunKey = "run::";
 
     // 특정 경기의 대기열에 유저 추가
     @Override
     public Long addToWaitQueue(int id, int gameId) {
         // 대기열 등록
-        String key = waitKey + gameId;
+        String key = baseWaitKey + gameId;
         double score = System.currentTimeMillis();
 
         redisTemplate.opsForZSet().add(key, String.valueOf(id), score);
@@ -35,42 +36,51 @@ public class WaitingServiceImpl implements WaitingService {
         return redisTemplate.opsForZSet().rank(key, String.valueOf(id));
     }
 
-    // 2초마다 일정 인원을 참가열로 이동시킨다
+    // 2초마다 일정 인원을 작업열로 이동시킨다
     @Scheduled(cron = "0/2 * * * * *")
     @Override
     public void moveToRunQueue() {
         log.info("[Waiting] 대기열 스케줄러 시작");
 
-        // 현재 REDIS에 존재하는 모든 대기열 KEY를 불러온다
-        Set<String> redisKeys = redisTemplate.keys(waitKey + "*");
+        // 현재 Redis에 존재하는 모든 대기열 Key를 불러온다
+        Set<String> redisKeys = redisTemplate.keys(baseWaitKey + "*");
         Iterator<String> it = redisKeys.iterator();
 
-        // 모든 대기열 ROOM에 대해 대기열 관리 스케줄링 수행
+        // 모든 대기열에 대해 스케줄링
         while (it.hasNext()) {
-            String key = it.next();
-            String game = key.split("::")[1];
+            String waitKey = it.next();
+            String gameId = waitKey.split("::")[1];
+            String runKey = baseRunKey + gameId;
 
-            // 이번 차례인 경우, 대기열에서 popAmount만큼을 꺼낸다
-            Set<String> waitQue = redisTemplate.opsForZSet().range(key, 0, popAmount);
-            log.info("Game {} Pass : {}", game, waitQue.size());
-            for (String s : waitQue) {
-                // 대기열에서 해당 유저 정보 삭제
-                redisTemplate.opsForZSet().remove(key, s);
+            // 작업열로 진입 가능한 Capability 확인
+            Long capability = popAmount - redisTemplate.opsForZSet().size(runKey);
+            if (capability > 0) {
+                // 이번 차례인 경우, 대기열에서 Capability만큼을 꺼낸다
+                Set<String> waitQue = redisTemplate.opsForZSet().range(waitKey, 0, capability - 1);
+                log.info("Game {} Pass : {}", gameId, waitQue.size());
 
-                // 본인 차례임을 표시하고 응답
-                WaitStateRes res = WaitStateRes.builder()
-                    .myTurn(true) // 본인 차례 여부
-                    .key(UUID.randomUUID().toString()) // 고유 랜덤 ID
-                    .rank(0)
-                    .build();
-                template.convertAndSend("/sub/id/" + s, res);
+                for (String s : waitQue) {
+                    // 대기열에서 해당 유저 정보 삭제
+                    redisTemplate.opsForZSet().remove(waitKey, s);
+
+                    // 작업열로 해당 유저 이동
+                    redisTemplate.opsForZSet().add(runKey, s, System.currentTimeMillis());
+
+                    // 본인 차례임을 표시하고 응답
+                    WaitStateRes res = WaitStateRes.builder()
+                        .myTurn(true) // 본인 차례 여부
+                        .key(UUID.randomUUID().toString()) // 고유 랜덤 ID
+                        .rank(0)
+                        .build();
+                    template.convertAndSend("/sub/id/" + s, res);
+                }
             }
 
             // 이번 차례가 아닌 경우, 그냥 값만 가져와서 반환한다
-            Set<String> leftQue = redisTemplate.opsForZSet().range(key, 0, -1);
-            log.info("Game {} left : {}", game, leftQue.size());
-            int idx = 1;
+            Set<String> leftQue = redisTemplate.opsForZSet().range(waitKey, 0, -1);
+            log.info("Game {} left : {}", gameId, leftQue.size());
 
+            int idx = 1;
             for (String s : leftQue) {
                 // 본인 차례가 아님을 표시하고 응답
                 WaitStateRes res = WaitStateRes.builder()
