9ee17f2a905fea2f9070c48a4329d9ab0021f48e
최민경 <kjjpyckjs@naver.com>
2024-03-28 02:30:17 +0900

    [FE] feat: 단체 선수 조회 api 연결 및  css 수정 #S10P22D211-266

--- Stickey-frontend/src/components/Profile/Group/PlayerList.tsx
+++ Stickey-frontend/src/components/Profile/Group/PlayerList.tsx
@@ -10,31 +10,38 @@ const PlayerList = () => {
 
   return (
     <div>
-    <div className="w-full flex flex-col justify-center gap-3 items-center px-4">
+    <div>
       {playerListInfo && playerListInfo?.data.playerResList.length > 0 ? (
         playerListInfo?.data.playerResList.map((item, id) => (
-          <div key={id} className="px-4">
-            <div className="flex items-center h-auto py-4 border-none rounded-[10px] bg-[#2E2E3D]">
-              <div className="w-full flex flex-row px-4 gap-3">
-                <div className="w-20 h-20 border border-none rounded-full bg-Stickey_Gray">프로필 사진</div>
-                <div className="flex flex-col w-[208px] h-[20px] gap-2 text-white">
-                  <div className="flex flex-row gap-1">
-                    <div className="flex flex-col justify-end">
-                      <p className="text-[8px]">{item.category}</p>
+          <div className="py-2 flex flex-col justify-center items-center" key={id}>
+            <div className="py-4 rounded-lg bg-[#2E2E3D]">
+              <div className="flex items-center">
+
+                {/* 선수 사진 */}
+                <img className="w-20 h-20 ml-8 mr-4 rounded-full" src={item.profile} />
+
+                {/*  선수 정보 */}
+                <div className="flex flex-col px-8 h-auto text-white pl-4">
+               
+                    <div className="flex items-center justify-start py-2">
+                      <p className="text-xs px-2">{item.category}</p>
+                      <p className="text-md px-2">{item.name}</p>
                     </div>
-                    <p className="text-[12px]">{item.name}</p>
-                  </div>
-                  <div className="border-b-[0.5px] w-full"></div>
-                  <div className="flex flex-col gap-2">
-                    <div className="flex flex-row">
-                      <p className="w-12 h-[10px] text-[8px] text-[#A0A0A0]">생년월일</p>
-                      <p className="w-12 h-[10px] text-[8px]">{item.birth}</p>
+                  
+                    <div className="border-b-2"></div>
+
+                    <div className="flex flex-col py-2">
+
+                      <div className="flex items-center">
+                        <p className="text-xs px-2 text-[#A0A0A0]">생년월일</p>
+                        <p className="text-sm px-2">{item.birth}</p>
+                      </div>
+
+                      <div className="flex items-center">
+                        <p className="text-xs px-2 text-[#A0A0A0]">선수설명</p>
+                        <div className="px-2 text-sm">{item.description}</div>
+                      </div>
                     </div>
-                    <div className="flex flex-row">
-                      <p className="w-12 h-10 text-[8px] text-[#A0A0A0]">선수 설명</p>
-                      <div className="flex flex-col gap-1">{item.description}</div>
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
