6dff37bd2e13d3995b37f8a55fd723752935800e
최민경 <kjjpyckjs@naver.com>
2024-03-29 01:11:20 +0900

    [FE] feat: 웹소켓 연결 로직 구현 #S10P22D211-273

--- Stickey-frontend/src/components/Book/WaittingModal.tsx
+++ Stickey-frontend/src/components/Book/WaittingModal.tsx
@@ -1,31 +1,71 @@
 import Modal from '../@common/Modal'
 import Waitting from '../../assets/image/Waitting.png'
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useTicketInfoStore } from '../../stores/useTicketInfoStore';
+import  { CompatClient, Stomp } from '@stomp/stompjs'
+import SockJS from 'sockjs-client';
 
 const WaittingModal = ({ onClose }: { onClose: () => void; }) => {
 
     const navigate = useNavigate(); 
     const ticketInfo = useTicketInfoStore((state) => state.modalData)
+    const client = useRef<CompatClient>();
 
     // 임시 대기열
     useEffect(() => {
-        const timer = setTimeout(() => {
-            onClose();
-                navigate(`/${ticketInfo?.id}/section`);
-        }, 3000);
+        const userId = 1; // 임의 유저
+        const gameId = ticketInfo?.id;
+    
+        const connectWebSocket = () => {
+            client.current = Stomp.over(() => {
+                const sock = new SockJS(`${import.meta.env.VITE_WEBSOCKET_URL}`);
+                console.log("연결시도?")
+                console.log(sock)
+                return sock;
+            });
+    
+            client.current?.connect({}, () => {
+                console.log('WebSocket 연결 성공');
+                client.current?.subscribe(
+                    `/sub/id/${userId}`,
+                    (message) => {
+                        const waitStateRes = JSON.parse(message.body);
+                        console.log("내 차례인지: " + waitStateRes.myTurn);
+                        console.log("대기열 순위: " + waitStateRes.rank);
+                        console.log("key: " + waitStateRes.key);
+                    }
+                );
+    
+                client.current?.send("/games/wait/enter", {}, JSON.stringify({"gameId": gameId, "id": userId}));
+            }, () => {
+                console.error('WebSocket 연결 실패');
+                setTimeout(connectWebSocket, 5000); // 5초 후에 재시도
+            });
+        };
+    
+        connectWebSocket();
 
-        return () => clearTimeout(timer); 
+        const timeout = setTimeout(() => {
+            onClose();
+            navigate(`/${gameId}/section`);
+        }, 6000); // 6초 후에 모달 닫고 페이지로 이동
+    
+        return () => {
+            if (client.current?.connected) {
+                client.current.disconnect();
+                console.log('WebSocket 연결 종료');
+            }
+            clearTimeout(timeout);
+        };
     }, [onClose, navigate, ticketInfo]);
     
-    console.log(ticketInfo)
-    
     return (
-        <Modal width="300px" height="auto" title="현재 대기순서" onClose={onClose}>
+        <Modal width="300px" height="auto" title="" onClose={onClose}>
             <div className="flex flex-col items-center px-4 pb-6 z-[10]">
                 <div className='flex flex-col items-center pb-2'>
                     <img className='h-20' src={Waitting} />
+                    <p className='text-sm py-2'>나의 대기</p>
                     <h1 className='text-4xl font-bold'>3562</h1>
                 </div>
 
