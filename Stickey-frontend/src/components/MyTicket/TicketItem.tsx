ee7716ada273acab0870e8b364e938d9368875bd
전찬혁 <cksgur5530@gmail.com>
2024-03-28 20:57:25 +0900

    [FE] feat: 마이티켓 조회 #S10P22D211-238

--- Stickey-frontend/src/components/MyTicket/TicketItem.tsx
+++ Stickey-frontend/src/components/MyTicket/TicketItem.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { ITicket } from '../../pages/MyTicket/MyTicketPage';
-import Poster from '../../assets/image/Poster.png'
 import TicketOpenModal from './TicketOpenModal';
 import dayjs from 'dayjs';
 
@@ -20,8 +19,8 @@ const TicketItem: React.FC<TicketItemProps> = ({ ticket }) => {
                 <div onClick={() => setIsModalOpen(true)} className="bg-gray-200 p-1 rounded-xl flex flex-col items-center">
                     <p>{ticket.category}{ticket.tokenId}</p>
                     <img className='h-44' src={ticket.gameImage} alt="gameImage"/>
-                        <p className="text-xs">{ dayjs(Number(ticket) * 1000).format("YYYY년 MM월 DD일 HH:mm:ss") }</p>
-                    <p className="font-bold">{ticket.zoneName} {ticket.seatNumber}</p>
+                    <p className="text-xs">{ dayjs(Number(ticket.gameStartTime) * 1000).format("YYYY/MM/DD HH:mm") }</p>
+                    <p className="font-bold">{ticket.zoneName} {ticket.seatNumber.toString()}</p>
                     <p className="text-sm">{ticket.stadium}</p>
                 </div>
 
