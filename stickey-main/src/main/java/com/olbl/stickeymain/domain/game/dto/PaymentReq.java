cc825c4857ad2f5f767e903a77a559882dfb756f
윤희원 <gmldnjs427@naver.com>
2024-03-29 00:28:44 +0900

    [BE] fix: PaymentReq userId필드 삭제 #S10P22D211-223

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/game/dto/PaymentReq.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/game/dto/PaymentReq.java
@@ -8,7 +8,6 @@ public class PaymentReq {
 
     //최종 좌석 상태  결제 가능한지 요청할때 받아야 하는 정보
     private List<Integer> seatNumbers;
-    private int userId;
     private int gameId;
     private int zoneId;
 
