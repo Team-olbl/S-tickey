96877d6960776af3d1c0d4faa8e24136b3a681ab
강정수 <bbbbqz@nate.com>
2024-03-28 10:24:20 +0900

    [FE] feat: 단체 회원가입 api 구현 #S10P22D211-288

---
+++ Stickey-frontend/src/service/OrganizationUser/api.ts
@@ -0,0 +1,11 @@
+import { axiosCommonInstance } from "../../apis/axiosInstance";
+import { APIResponse } from "../../types/model";
+
+export const signup = async (info: FormData):Promise<APIResponse<string>> => {
+  const {data} = await axiosCommonInstance.post(`users/signup/organization`, info, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+  })
+  return data
+}
