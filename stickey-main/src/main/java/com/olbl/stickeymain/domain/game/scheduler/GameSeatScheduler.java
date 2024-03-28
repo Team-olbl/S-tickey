675b73d9503e1453bdc90c38c44e53f67e792fad
윤희원 <gmldnjs427@naver.com>
2024-03-28 16:44:58 +0900

    [BE] fix: 스케줄러 redis에 db에서 가져온 정보 넣게 수정 #S10P22D211-223

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/game/scheduler/GameSeatScheduler.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/game/scheduler/GameSeatScheduler.java
@@ -1,9 +1,10 @@
 package com.olbl.stickeymain.domain.game.scheduler;
 
 import com.olbl.stickeymain.domain.game.entity.Game;
+import com.olbl.stickeymain.domain.game.entity.GameSeat;
 import com.olbl.stickeymain.domain.game.repository.GameRepository;
+import com.olbl.stickeymain.domain.game.repository.GameSeatRepository;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +18,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class GameSeatScheduler {
 
+    private final GameSeatRepository gameSeatRepository;
     private final GameRepository gameRepository;
     private final StringRedisTemplate redisTemplate;
 
-    //TODO: 테스트 이후에 시간 늘리기
-    @Scheduled(fixedRate = 60000) // 1분마다 실행
+    //TODO: 테스트 이후에 시간 수정하기
+    @Scheduled(cron = "0 48 16 * * *") //오후 4시 45분에 실행
     public void checkAndExecuteTask() {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime twentyMinutesFromNow = now.plusMinutes(20);
@@ -33,21 +35,16 @@ public class GameSeatScheduler {
             //TODO: 예매 오픈 20분 전 알림 보내기
             //TODO: gameSeat 테이블 세팅 (spring batch)
 
+            List<GameSeat> gameSeatList = gameSeatRepository.findByGameId(game.getId());
+
             //redis에 좌석 정보 넣기
             ListOperations<String, String> listOps = redisTemplate.opsForList();
-            Integer[] zones = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; //구역 id
-            int seatsPerSection = 30; //구역 별 좌석 수로 변경
 
-            for (Integer zone : zones) {
-                String key = "game:" + game.getId() + ":zone:" + zone;
-                Boolean exists = redisTemplate.hasKey(key); //키가 이미 존재하는지 확인
-                if (Boolean.FALSE.equals(exists)) {
-                    List<String> seats = new ArrayList<>();
-                    for (int seat = 1; seat <= seatsPerSection; seat++) {
-                        seats.add("AVAILABLE"); //초기 상태 AVAILABLE
-                    }
-                    listOps.rightPushAll(key, seats); // Redis에 좌석 상태 정보 저장
-                }
+            for (GameSeat gameSeat : gameSeatList) {
+                String key = "game:" + gameSeat.getGame().getId() + ":zone:" + gameSeat.getZoneId();
+
+                String seat = gameSeat.getStatus().toString();
+                listOps.rightPush(key, seat);
             }
         }
     }
