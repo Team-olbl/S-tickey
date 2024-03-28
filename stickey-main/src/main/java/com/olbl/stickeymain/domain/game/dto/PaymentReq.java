7185e7c4581d9a6c9de7a5d8b4148d18140a9872
윤희원 <gmldnjs427@gmail.com>
2024-03-29 01:54:53 +0900

    [BE] feat: 환불 시 좌석 상태 변경 기능 추가 #S10P22D211-275

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/game/dto/PaymentReq.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/game/dto/PaymentReq.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 public class PaymentReq {
 
-    //최종 좌석 상태  결제 가능한지 요청할때 받아야 하는 정보
     private List<Integer> seatNumbers;
     private int gameId;
     private int zoneId;
+    private Boolean isRefund; // 환불:true, 결제:false
 
 }
