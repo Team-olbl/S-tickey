f9f357ab5ce41932b4726ae6bc2079393cea18f5
강정수 <bbbbqz@nate.com>
2024-03-28 22:18:31 +0900

    [FE] feat: 유저상태관리 #S10P22D211-264

--- Stickey-frontend/src/apis/interceptors.ts
+++ Stickey-frontend/src/apis/interceptors.ts
@@ -1,28 +1,13 @@
-// import { InternalAxiosRequestConfig } from 'axios';
-
-// const setAuthorization = (config: InternalAxiosRequestConfig) => {
-//   const userStore = localStorage.getItem('user-store');
-//   if (userStore) { 
-//     // const accessToken = JSON.parse(userStore).state.accessToken;
-//     const accessToken = import.meta.env.VITE_TOKEN;
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//   }
-//   return config;
-// };
-
-// export default setAuthorization;
-
 import { InternalAxiosRequestConfig } from 'axios';
 
 const setAuthorization = (config: InternalAxiosRequestConfig) => {
-  // const accessToken = JSON.parse(localStorage.getItem('user-store') || '').state.accessToken;
-  const accessToken = import.meta.env.VITE_TOKEN;
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
+  const userStore = localStorage.getItem('user-store');
+  if (userStore) { 
+    const accessToken = JSON.parse(userStore).state.accessToken;
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
   }
-
   return config;
 };
 
