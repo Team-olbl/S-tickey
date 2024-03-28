96ab38ef741f2cdd1de70af8bbc0bd0985b7399f
윤희원 <gmldnjs427@gmail.com>
2024-03-28 23:51:00 +0900

    [BE] feat: 결제 시 좌석 상태 변경 api 구현 #S10P22D211-223

---
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/game/dto/PaymentReq.java
@@ -0,0 +1,15 @@
+package com.olbl.stickeymain.domain.game.dto;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class PaymentReq {
+
+    //최종 좌석 상태  결제 가능한지 요청할때 받아야 하는 정보
+    private List<Integer> seatNumbers;
+    private int userId;
+    private int gameId;
+    private int zoneId;
+
+}
