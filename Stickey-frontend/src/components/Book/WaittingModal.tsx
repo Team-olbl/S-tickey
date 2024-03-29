79f8d11e95da31cb3da9e70bcb48999dc3a3c766
최민경 <kjjpyckjs@gmail.com>
2024-03-29 21:42:23 +0900

    [FE] feat: 예매 대기열 websocket 구현 #S10P22D211-273

--- Stickey-frontend/src/components/Book/WaittingModal.tsx
+++ Stickey-frontend/src/components/Book/WaittingModal.tsx
@@ -1,61 +1,58 @@
 import Modal from '../@common/Modal';
 import Waitting from '../../assets/image/Waitting.png';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTicketInfoStore } from '../../stores/useTicketInfoStore';
 import { Client } from '@stomp/stompjs';
 import userStore from '../../stores/userStore';
+import { useNavigate } from 'react-router';
+import Loading from '../../assets/image/Loading.gif'
 
 const WaittingModal = ({ onClose }: { onClose: () => void; }) => {
     const ticketInfo = useTicketInfoStore((state) => state.modalData);
     const { id: userId } = userStore();
-    const [waitNumbers, setWaitNumbers] = useState<number[]>([]);
-    const [currentNumberIndex, setCurrentNumberIndex] = useState(0);
     const [client, setClient] = useState<Client | null>(null);
+    const parsedMessageRef = useRef<number>(0);
+    const navigate = useNavigate()
 
-    console.log(setWaitNumbers, setCurrentNumberIndex, client)
-
-    useEffect(() => {
-    
-        const newClient = new Client({
-          brokerURL: 'ws://j10d211.p.ssafy.io:9091/api/reserve',
-        //   connectHeaders: {
-        //     Authorization: `Bearer ${accessToken}`,
-        //   },
-          beforeConnect: () => {
-            console.log("Connecting to WebSocket");
-          },
-          onConnect: () => {
-            console.log("Connected to WebSocket");
-          },
-          onDisconnect: () => {
-            console.log("Disconnected from WebSocket");
-          },
-          onWebSocketClose: (closeEvent) => {
-            console.log("WebSocket closed", closeEvent);
-          },
-          onWebSocketError: (error) => {
-            console.log("WebSocket error: ", error);
-          },
-          heartbeatIncoming: 0,
-          heartbeatOutgoing: 0,
-        });
-    
-        setClient(newClient);
-        newClient.activate();
-      }, []);
 
     useEffect(() => {
         const newClient = new Client();
         newClient.configure({
-            brokerURL: 'ws://j10d211.p.ssafy.io:9091/api/reserve',
+            brokerURL: `${import.meta.env.VITE_WEBSOCKET_URL}`,
             onConnect: () => {
                 newClient.subscribe(
                     `/sub/id/${userId}`,
                     message => {
                         const parsedMessage = JSON.parse(message.body);
-                        console.log(parsedMessage);
+                        parsedMessageRef.current = parsedMessage.rank;
+                        console.log(parsedMessage.myTurn);
+                        console.log(parsedMessage.rank);
+                        console.log(parsedMessage.key);
+
+                        // rank 값이 0이면 모달 닫고 취소 메시지 전송 및 페이지 이동
+                        if (parsedMessageRef.current === 0) {
+                            onClose(); // 모달 닫기
+                            if (client) {
+                                client.publish({
+                                    destination: '/games/wait/cancel',
+                                    body: undefined,
+                                });
+                            }
+                            navigate(`/${ticketInfo?.id}/section`); // 페이지 이동
+                        }
                     },
                 );
+
+                // '/games/wait/enter' 
+                const message = {
+                    gameId: ticketInfo?.id,
+                    id: userId
+                };
+                newClient.publish({
+                    destination: '/games/wait/enter',
+                    body: JSON.stringify(message),
+                });
+
                 console.log('웹소켓 연결 확인');
             },
             onDisconnect: () => {
@@ -71,27 +68,43 @@ const WaittingModal = ({ onClose }: { onClose: () => void; }) => {
         };
     }, [ticketInfo, userId]);
 
+    useEffect(() => {
+        const handleUnload = () => {
+            if (client) {
+                client.publish({
+                    destination: '/games/wait/cancel',
+                    body: undefined,
+                });
+            }
+        };
+
+        window.addEventListener('beforeunload', handleUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleUnload);
+            handleUnload();
+        };
+    }, [client, ticketInfo, userId]);
 
     return (
         <Modal width="300px" height="auto" title="" onClose={onClose}>
             <div className="flex flex-col items-center px-4 pb-6 z-[10]">
-                <div className='flex flex-col items-center pb-2'>
-                    <img className='h-20' src={Waitting} alt="Waitting" />
-                    <p className='text-sm py-2'>나의 대기</p>
-                    <h1 className='text-4xl font-bold'>{waitNumbers[currentNumberIndex]}</h1>
+                <div className='flex flex-col items-center'>
+                    <img className='h-12' src={Waitting} alt="Waitting" />
+                    <p className='text-sm pt-2'>나의 대기</p>
+                    <h1 className='text-4xl font-bold'>{parsedMessageRef?.current}</h1>
                 </div>
 
-                <div className="pt-1 py-4 relative">
-                    <div className="w-48 h-2 rounded-xl"></div>
-                    <div className="absolute top-0 left-0 h-2 bg-Stickey_Main rounded-xl" style={{ width: `${(currentNumberIndex + 1) * (100 / waitNumbers.length)}%` }}></div>
+                <div >
+                    <img className='w-16' src={Loading} />
                 </div>
         
                 <div className='flex flex-col px-4'>
-                    <div className='text-[8px] text-center'>
+                    <div className='text-[10px] text-center'>
                         <p>현재 접속 인원이 많아 대기중입니다.</p>
                         <p>잠시만 기다려주시면 예매 페이지로 연결됩니다.</p>
                     </div>
-                    <div className='text-[10px] font-semibold text-center py-2'>
+                    <div className='text-[12px] font-semibold text-center py-2'>
                         <p>새로고침 하거나 재접속 히시면</p>
                         <p>대기순서가 초기화됩니다.</p>
                     </div>
