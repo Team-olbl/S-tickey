890682ee4ec7f15768bd1ff61406e30aa12a6d66
전찬혁 <cksgur5530@gmail.com>
2024-03-31 01:50:59 +0900

    [FE] 후원글승인시 블록체인 연결, 관리자 네비게이트 #S10P22D211-297

--- Stickey-frontend/src/pages/Admin/AdminPage.tsx
+++ Stickey-frontend/src/pages/Admin/AdminPage.tsx
@@ -3,95 +3,77 @@ import unlock from  '../../assets/image/Unlocked.png'
 import SignupApprove from "../../components/Admin/SignupApprove";
 import PostApprove from "../../components/Admin/PostApprove";
 import LogoutModal from "../../components/Login/LogoutModal";
+import { useProfile } from "../../hooks/Profile/useProfile";
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
+import NavigationBar from "../../components/@common/NavigationBar";
 
 
 const AdminPage = () => {
-  const leftTab: string = '단체 회원 승인';
-  const rightTab: string = '후원글 승인';
-  const [activeTab, setActiveTab] = useState(leftTab);
-  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+    const { useGetProfile } = useProfile();
 
-  const handleLeftTabClick = () => {
-    setActiveTab(leftTab);
-  };
+    const leftTab: string = '단체 회원 승인';
+    const rightTab: string = '후원글 승인';
+    const [activeTab, setActiveTab] = useState(leftTab);
+    const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+    const { data : profileRes, isSuccess } = useGetProfile();
+    
+    if (isSuccess) {
+        if (profileRes.data.role !== 'ADMIN') {
+            toast.warn("잘못된 접근입니다.");
+            return <Navigate to='/'></Navigate>
+        }
+    }
 
-  const handleRightTabClick = () => {
-    setActiveTab(rightTab);
-  };
+    const handleLeftTabClick = () => {
+        setActiveTab(leftTab);
+    };
 
-  return(
-    <>
-        {/* 관리자페이지 안내 */}
-        <div className="flex justify-between items-center pt-4 px-8">
-            <p className="text-xl py-6 text-white">Stickey 관리자 페이지 입니다</p>
-            <div className="text-white flex items-center border rounded-lg p-2 cursor-pointer" onClick={() => setIsLogoutModalOpen(true)}>
-                <img className="w-8 mr-2" src={unlock} />
-                <p>로그아웃</p>
-            </div>
-        </div>
-        
-        {/* 탭바 */}
-        <div className="text-sm w-full flex relative pb-2 text-center pt-8 max-w-[500px] ">
-            <button
-                onClick={handleLeftTabClick}
-                className={activeTab === leftTab ? 'font-bold flex-1 text-Stickey_Main' : 'text-gray-500 flex-1'}
-            >
-                <p>{leftTab}</p>
-            </button>
-            <button
-                onClick={handleRightTabClick}
-                className={activeTab === rightTab ? 'font-bold flex-1 text-Stickey_Main' : 'text-gray-500 flex-1'}
-            >
-                <p>{rightTab}</p>
-            </button>
-            <button className="absolute bottom-0 left-0 w-full h-[1px] bg-gray-500">
-                <div
-                className={`${activeTab !== leftTab ? 'left-1/2' : 'left-0'} duration-500 ease-in-out relative bottom-[1px] z-10 w-1/2 h-[2px] bg-Stickey_Main`}
-                ></div>
-            </button>
-        </div>
+    const handleRightTabClick = () => {
+        setActiveTab(rightTab);
+    };
 
-        {activeTab === leftTab && <SignupApprove />}
-      {activeTab === rightTab && <PostApprove />}
-      {isLogoutModalOpen && <LogoutModal onClose={() => setIsLogoutModalOpen(false)}/>}
-  </>
-  )
+    return(
+        <>  
+            {isSuccess &&
+            <>
+                {/* 관리자페이지 안내 */}
+                <div className="flex justify-between items-center pt-4 px-8">
+                    <p className="text-xl py-6 text-white">Stickey 관리자 페이지 입니다</p>
+                    <div className="text-white flex items-center border rounded-lg p-2 cursor-pointer" onClick={() => setIsLogoutModalOpen(true)}>
+                        <img className="w-8 mr-2" src={unlock} />
+                        <p>로그아웃</p>
+                    </div>
+                </div>
+                
+                {/* 탭바 */}
+                <div className="text-sm w-full flex relative pb-2 text-center pt-8 max-w-[500px] ">
+                    <button
+                        onClick={handleLeftTabClick}
+                        className={activeTab === leftTab ? 'font-bold flex-1 text-Stickey_Main' : 'text-gray-500 flex-1'}
+                    >
+                        <p>{leftTab}</p>
+                    </button>
+                    <button
+                        onClick={handleRightTabClick}
+                        className={activeTab === rightTab ? 'font-bold flex-1 text-Stickey_Main' : 'text-gray-500 flex-1'}
+                    >
+                        <p>{rightTab}</p>
+                    </button>
+                    <button className="absolute bottom-0 left-0 w-full h-[1px] bg-gray-500">
+                        <div
+                        className={`${activeTab !== leftTab ? 'left-1/2' : 'left-0'} duration-500 ease-in-out relative bottom-[1px] z-10 w-1/2 h-[2px] bg-Stickey_Main`}
+                        ></div>
+                    </button>
+                </div>
+
+                {activeTab === leftTab && <SignupApprove />}
+                {activeTab === rightTab && <PostApprove />}
+                { isLogoutModalOpen && <LogoutModal onClose={() => setIsLogoutModalOpen(false)} /> }
+                </>}
+            <NavigationBar />
+        </>
+    )
 }
 
 export default AdminPage;
-
-
-// // 더미
-// const SignupdummyData: ISignupResProps = {
-//     signUpResList: [
-//       { name: "최민경", id: 1, profileImage: null },
-//       { name: "전찬혁", id: 2, profileImage: null },
-//       { name: "류지윤", id: 3, profileImage: null },
-//       { name: "최부광", id: 4, profileImage: null },
-//     ],
-//     count: 4
-//   };
-
-// const PostdummyData: IPostApproveProps = {
-//     waitingSupportRes: [
-//       {
-//         id: 1,
-//         title: "ㅇㅇ중학교 배구 유망주 체육관 수리 후원",
-//         organization: {
-//           name: "룰루랄라협회",
-//           id: 1,
-//           profileImage: "C:\\Users\\SSAFY\\Desktop\\heewon\\aaa.jpg"
-//         }
-//       },
-//       {
-//         id: 2,
-//         title: "ㅇㅇ고등학교 단체 축구 유니폼 기부",
-//         organization: {
-//           name: "하기싫다키키",
-//           id: 2,
-//           profileImage: "C:\\Users\\SSAFY\\Desktop\\heewon\\aaa.jpg"
-//         }
-//       }
-//     ],
-//     count: 2
-//   };
