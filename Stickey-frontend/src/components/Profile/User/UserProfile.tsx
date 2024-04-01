5a06ed57d0c300b358ba8d2a203d5977ae8fb653
최민경 <kjjpyckjs@naver.com>
2024-04-01 19:16:45 +0900

    [FE] style: 마이페이지 프로필 밋 지갑 수정 #S10P22D211-305

--- Stickey-frontend/src/components/Profile/User/UserProfile.tsx
+++ Stickey-frontend/src/components/Profile/User/UserProfile.tsx
@@ -10,15 +10,15 @@ const UserProfile = ({ userInfo }: { userInfo: IUserProfile | null }) => {
     <>
     {/* 개인 유저 프로필 */}
     <div className="flex justify-center">
-        <div className="flex flex-row gap-3 max-w-[500px] w-full h-[60px] border-none px-6">
-          <div className="w-[56px] h-[56px] rounded-full bg-white">
-            <img className="w-[56px] h-[56px] rounded-full" src={userInfo.profileImage} />
+        <div className="flex flex-row items-center gap-3 max-w-[500px] w-full h-auto border-none px-6">
+          <div className="w-20 h-20 rounded-full bg-white">
+            <img className="w-20 h-20 rounded-full" src={userInfo.profileImage} />
           </div>
           <div className="flex flex-col gap-2">
-            <p className="text-[20px] text-white font-semibold">{userInfo.name}</p>
+            <p className="text-2xl text-white font-semibold">{userInfo.name}</p>
             <div className="flex flex-row">
             {userInfo?.preference.map((team: IPreTeamSimpleRes, id: number) => (
-              <div key={id} className="p-1 px-2 border border-none font-bold bg-[#E4E4E4] text-[#5959E7] text-[8px] flex items-center justify-center rounded-xl mr-2">
+              <div key={id} className="p-1 px-2 border border-none bg-[#E4E4E4] text-[#5959E7] text-xs flex items-center justify-center rounded-xl mr-2">
                 <p className="">{team.sportsClubName}</p>
               </div>
             ))}
@@ -28,8 +28,8 @@ const UserProfile = ({ userInfo }: { userInfo: IUserProfile | null }) => {
       </div>
 
       {/* 지갑 */}
-      <div className="flex justify-center pt-6 px-6">
-        <div className="w-full pb-4">
+      <div className="flex justify-center pt-4 px-4">
+        <div className="w-full p-4">
           <Wallet />
         </div>
       </div>
