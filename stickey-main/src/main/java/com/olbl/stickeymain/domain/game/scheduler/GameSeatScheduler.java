4db3c2d180edca5fd21a5372f973c2ed0c073a50
윤희원 <gmldnjs427@naver.com>
2024-04-01 20:28:37 +0900

    [BE] fix: redis 저장 시간 계산 수정

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/game/scheduler/GameSeatScheduler.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/game/scheduler/GameSeatScheduler.java
@@ -4,6 +4,7 @@ import com.olbl.stickeymain.domain.game.entity.Game;
 import com.olbl.stickeymain.domain.game.entity.GameSeat;
 import com.olbl.stickeymain.domain.game.repository.GameRepository;
 import com.olbl.stickeymain.domain.game.repository.GameSeatRepository;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -23,7 +24,7 @@ public class GameSeatScheduler {
     private final GameRepository gameRepository;
     private final StringRedisTemplate redisTemplate;
 
-    @Scheduled(cron = "0 10 20 * * *") //매일 오후 20시 실행
+    @Scheduled(cron = "0 30 20 * * *") //매일 오후 20시 실행
     public void checkAndExecuteTask() {
 
         LocalDateTime now = LocalDateTime.now();
@@ -32,19 +33,18 @@ public class GameSeatScheduler {
         List<Game> upcomingGames = gameRepository.findByBookStartTimeBetween(now,
             OneHourAfterFromNow); // 하루 뒤에 시작되는 경기 정보 가져오기
 
-        log.info("게임 있나:" + upcomingGames.getFirst().getId());
-
         for (Game game : upcomingGames) {
             List<GameSeat> gameSeatList = gameSeatRepository.findByGameId(game.getId());
             LocalDateTime gameStartTime = game.getGameStartTime();
 
-            int expire = gameStartTime.getNano() - now.getNano();
+            Duration duration = Duration.between(now, gameStartTime);
+            long expire = duration.toNanos();
 
             //redis에 좌석 정보 넣기
             ListOperations<String, String> listOps = redisTemplate.opsForList();
 
             for (GameSeat gameSeat : gameSeatList) {
-                String key = "game:" + gameSeat.getGame().getId() + ":zone:" + gameSeat.getZoneId();
+                String key = "game:" + game.getId() + ":zone:" + gameSeat.getZoneId();
                 String seat = gameSeat.getStatus().toString();
 
                 listOps.rightPush(key, seat);
