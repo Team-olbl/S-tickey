5d83ac01e3fa0768c6d6c25c6f2c5cb90db79a5a
jiyun3016 <jiyun3016@gmail.com>
2024-04-01 17:23:29 +0900

    [BE] test: test 코드 제거 #S10P22D211-248

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/notify/service/NotifyService.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/notify/service/NotifyService.java
@@ -128,8 +128,7 @@ public class NotifyService {
     }
 
     @Transactional
-//    @Scheduled(fixedRate = 3600000)
-    @Scheduled(fixedRate = 30000) //test
+    @Scheduled(fixedRate = 3600000)
     public void notifyGameUpdate() {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime oneHourLater = now.plusHours(1);
