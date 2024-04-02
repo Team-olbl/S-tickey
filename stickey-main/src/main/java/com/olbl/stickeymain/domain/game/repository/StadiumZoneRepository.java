febf555da97211f0a8c4a7496d278d5438749133
윤희원 <gmldnjs427@gmail.com>
2024-04-02 15:03:25 +0900

    [BE] feat: 경기장 구역 별 좌석 정보 등록 api 구현 #S10P22D211-345

--- stickey-main/src/main/java/com/olbl/stickeymain/domain/game/repository/StadiumZoneRepository.java
+++ stickey-main/src/main/java/com/olbl/stickeymain/domain/game/repository/StadiumZoneRepository.java
@@ -1,9 +1,12 @@
 package com.olbl.stickeymain.domain.game.repository;
 
 import com.olbl.stickeymain.domain.game.entity.StadiumZone;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StadiumZoneRepository extends JpaRepository<StadiumZone, Integer>,
     StadiumZoneRepositoryQuerydsl {
+
+    List<StadiumZone> findAllByStadiumId(int id);
 
 }
