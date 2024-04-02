f3042ef791c1cc8e635ae447d29ac4a109148cd7
최민경 <kjjpyckjs@naver.com>
2024-04-02 10:35:09 +0900

    [FE] style: 네비게이션 바 및 글씨 크기 조정 #S10P22D211-313

--- Stickey-frontend/src/components/@common/NavigationBar.tsx
+++ Stickey-frontend/src/components/@common/NavigationBar.tsx
@@ -42,34 +42,34 @@ const NavigationBar = () => {
   }
 
   return (
-    <div className="fixed bottom-0 max-w-[500px] z-[2] w-full h-[52px] border-t-[0.5px] bg-Stickey_BGC border-white flex flex-row  justify-center gap-[56px] items-center ">
+    <div className="fixed pb-1 bottom-0 max-w-[500px] z-[2] w-full h-[62px] border-t-[0.5px] bg-Stickey_BGC border-white flex flex-row  justify-center gap-[56px] items-center ">
       <div className="flex flex-col items-center gap-0" onClick={() => handleTabClick("home")}>
         <img
           src={pathname === "/" ? FilledHouse : House}
           className="w-[32px] h-[32px]"
         />
-        <p className="text-white text-center text-[8px]">홈</p>
+        <p className="text-white text-center text-[10px]">홈</p>
       </div>
       <div className="flex flex-col items-center" onClick={() => handleTabClick("sponsor")}>
         <img 
           src={pathname === "/sponsor" ? FilledTree : Tree} 
           className="w-[32px] h-[32px]"
         />
-        <p className="text-white text-center text-[8px]">후원</p>
+        <p className="text-white text-center text-[10px]">후원</p>
       </div>
       <div className="flex flex-col items-center" onClick={() => handleTabClick("mytickets")}>
         <img
           src={pathname === "/mytickets" ? FilledTicket : Ticket}
           className="w-[32px] h-[32px]"
         />
-        <p className="text-white text-center text-[8px]">티켓</p>
+        <p className="text-white text-center text-[10px]">티켓</p>
       </div>
       <div className="flex flex-col items-center" onClick={() => handleTabClick("profile")}>
         <img 
           src={pathname === "/profile" ? FilledUser : User}
           className="w-[32px] h-[32px]"
         />
-        <p className="text-white text-center text-[8px]">마이</p>
+        <p className="text-white text-center text-[10px]">마이</p>
       </div>
     </div>
   );
