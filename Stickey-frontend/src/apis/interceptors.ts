59ff6d3410499403fde877811eee28bf946af10b
전찬혁 <cksgur5530@gmail.com>
2024-04-04 00:39:09 +0900

    [FE] fix: accessToken

--- Stickey-frontend/src/apis/interceptors.ts
+++ Stickey-frontend/src/apis/interceptors.ts
@@ -1,14 +1,11 @@
 import { InternalAxiosRequestConfig } from 'axios';
 
 const setAuthorization = (config: InternalAxiosRequestConfig) => {
-  // const userStore = localStorage.getItem('user-store');
-  // if (userStore) {
-  //   const accessToken = JSON.parse(userStore).state.accessToken;
-  const accessToken = localStorage.getItem('accessToken');
-  if (accessToken) {
+  const userStore = localStorage.getItem('user-store');
+  if (userStore) {
+    const accessToken = JSON.parse(userStore).state.accessToken;
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
-  // }
   return config;
 };
 
