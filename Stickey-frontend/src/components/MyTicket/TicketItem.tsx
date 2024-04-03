f6b6ad0ed3b4c76e1a6aa0b55e2591cf02edb12f
강정수 <bbbbqz@nate.com>
2024-04-03 16:21:40 +0900

    [FE] style: 티켓 css 크기 조정 #S10P22D211-318

--- Stickey-frontend/src/components/MyTicket/TicketItem.tsx
+++ Stickey-frontend/src/components/MyTicket/TicketItem.tsx
@@ -49,20 +49,20 @@ const TicketItem: React.FC<TicketItemProps> = ({ ticket, getData }) => {
     return (
         <>
         <div className={`py-2 m-1`} ref={itemRef}>
-            <div key={ticket.tokenId}>
-                {/* 카드 모양 만들기 */}
-                <div onClick={() => setIsModalOpen(true)} className={`background${ticket.backgroundId} p-1 rounded-xl flex flex-col items-center`}>
-                    <p>{ticket.category}{ticket.tokenId}</p>
-                    <img className='h-44' src={ticket.gameImage} alt="gameImage"/>
-                    <p className="text-xs">{ dayjs(Number(ticket.gameStartTime) * 1000).format("YYYY/MM/DD HH:mm") }</p>
-                    <p className="font-bold">{ticket.zoneName} {ticket.seatNumber.toString()}</p>
-                    <p className="text-sm">{ticket.stadium}</p>
-                </div>
+        <div key={ticket.tokenId}>
+            {/* 카드 모양 만들기 */}
+            <div onClick={() => setIsModalOpen(true)} className={`background${ticket.backgroundId} p-1 rounded-xl flex flex-col items-center`}>
+                <p>{ticket.category}{ticket.tokenId}</p>
+                <img className='h-44' src={ticket.gameImage} alt="gameImage"/>
+                <p className="text-xs">{ dayjs(Number(ticket.gameStartTime) * 1000).format("YYYY/MM/DD HH:mm") }</p>
+                <p className="font-bold">{ticket.zoneName} {ticket.seatNumber.toString()}</p>
+                <p className="text-sm">{ticket.stadium}</p>
             </div>
         </div>
-            {isModalOpen &&
-                <TicketOpenModal ticket={ticket} onClose={() => setIsModalOpen(false)} getData={getData} />
-            }
+        </div>
+        {isModalOpen &&
+            <TicketOpenModal ticket={ticket} onClose={() => setIsModalOpen(false)} getData={getData} />
+        }
     </>
     );
 };
--- Stickey-frontend/src/components/MyTicket/TicketOpenModal.tsx
+++ Stickey-frontend/src/components/MyTicket/TicketOpenModal.tsx
@@ -126,59 +126,66 @@ const TicketOpenModal: React.FC<TicketOpenModalProps> = ({ ticket, onClose, getD
 
 
     return (
-        <div className="z-[1] w-full fixed inset-0 bg-black/70 flex justify-center items-center">
-            {/* modal wrapper */}
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg 100vh"  ref={scope}>
-                {onLoad &&
-                    <div className='flex justify-center mb-2 items-center fixed left-0 right-0 rounded-1'
-                        style={{ transform : "translateY(-110%)" }}>
-                        <img src={Warning} alt="" className="w-8"/>
-                        <div>
-                            <p className='ml-2 text-sm text-white h-full '>캡처한 이미지로는 입장이 불가능합니다.</p>
-                        </div>
-                    </div>}
-                {/* modal */}
-                <div ref={containerRef} className="w-[300px] relative flex flex-col text-center items-center">
-                    <div ref={overlayRef} className={`filter${ticket.filterId}`}></div>
-                    <div>
-                        <div className={`w-[300px] background${ticket.backgroundId} rounded-b-lg p-2 font-semibold`}>
-                            <p>{ticket.homeTeam} vs {ticket.awayTeam}</p>
-                        </div>
-                        <div className={` background${ticket.backgroundId} rounded-2xl`}>
-                            <div className='flex ml-2'>
-                        </div>
-                        {!isQR ?
-                        <>
-                            <img className={`w-[300px] rounded-3xl p-4 `} src={ticket.gameImage} alt="Game" />
-                            <div className="absolute bottom-[200px] right-6">
-                                <img onClick={handleEditClick} className="bg-white/50 rounded-full p-2" src={Edit} alt="Edit" />
+        <>
+        <div className='fixed top-0 w-full h-screen max-w-[500px] bottom-0 bg-black/70 z-[2]'>
+                
+                {/* modal wrapper */}
+                <div className="flex justify-center items-center rounded-lg pt-[20vh]"  ref={scope}>
+                    <div className='flex justify-center'>
+                        {onLoad &&
+                            <div className='flex justify-center mb-2 items-center fixed left-0 right-0 rounded-1'
+                                style={{ transform : "translateY(-110%)" }}>
+                                <img src={Warning} alt="" className="w-8"/>
+                                <div>
+                                    <p className='ml-2 text-sm text-white h-full '>캡처한 이미지로는 입장이 불가능합니다.</p>
+                                </div>
+                            </div>}
+                        {/* modal */}
+                        <div className='flex flex-col justify-center items-center w-[80%]'>
+                            <div ref={containerRef} className="text-center">
+                                <div ref={overlayRef} className={`filter${ticket.filterId}`}></div>
+                                <div>
+                                    <div className={`background${ticket.backgroundId} rounded-b-lg p-2 font-semibold`}>
+                                        <p>{ticket.homeTeam} vs {ticket.awayTeam}</p>
+                                    </div>
+                                    <div className={` background${ticket.backgroundId} rounded-2xl`}>
+                                        <div className='flex ml-2'>
+                                    </div>
+                                    {!isQR ?
+                                    <div className='flex justify-center max-w-[300px] h-auto'>
+                                        <img className={`rounded-3xl p-4 w-full`} src={ticket.gameImage} alt="Game" />
+                                        <img onClick={handleEditClick} className="fixed right-24 mt-6 bg-white/70 w-8 h-8 rounded-full p-1" src={Edit} alt="Edit" />
+                                    </div>
+                                    :
+                                    <div>
+                                        <img className={`rounded-3xl p-4 ${onLoad && `p-12`}`} src={qr} onLoad={handleLoad} />
+                                        {onLoad && <GetTime createQR={createQR}></GetTime>}
+                                    </div>
+                                    }
+                                    </div>
+                                    <div className={`background${ticket.backgroundId} rounded-t-lg text-center p-2`}>
+                                        <p className='text-sm p-1'>{dayjs(Number(ticket.gameStartTime) * 1000).format('YY/MM/DD HH:mm')} {ticket.stadium}</p>
+                                        <p className="font-bold text-2xl">{ticket.zoneName} {ticket.seatNumber}번 좌석</p>
+                                        <p>결제금액 : {toEther(ticket.price)} ETH</p>
+                                        <div className="px-4 py-3 flex justify-center">
+                                            <button className="z-[1] bg-gray-400 mx-1 p-2 rounded-md text-white"  onClick={handleQR}>QR 전환</button>
+                                            <button onClick={handleRefund} className="z-[1] bg-gray-400 mx-1 p-2 rounded-md text-white">환불하기</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </>
-                        :
-                        <>
-                            <img className="w-[300px] rounded-3xl p-4" src={qr} onLoad={handleLoad} />
-                            {onLoad && <GetTime createQR={createQR}></GetTime>}
-                        </>
-                        }
-                        </div>
-                        <div className={`background${ticket.backgroundId} rounded-t-lg w-[300px] text-center py-2`}>
-                            <p>{dayjs(Number(ticket.gameStartTime) * 1000).format('YYYY/MM/DD HH:mm')} {ticket.stadium}</p>
-                            <p className="font-bold text-2xl">{ticket.zoneName} {ticket.seatNumber}번 좌석</p>
-                            <p>결제금액 : {toEther(ticket.price)} ETH</p>
-                            <div className="px-4 py-3 flex justify-center">
-                                <button className="z-[1] bg-gray-400 w-1/2 mx-1 p-2 rounded-md text-white font-bold"  onClick={handleQR}>QR전환</button>
-                                <button onClick={handleRefund} className="z-[1] bg-gray-400 w-1/2 mx-1 p-2 rounded-md text-white font-bold">환불하기</button>
-                            </div>
+                    
+                    <div className="w-full flex justify-center mt-4">
+                        <div onClick={onClose} className="flex justify-center items-center h-12 w-12 bg-white rounded-full cursor-pointer">
+                            <img className="w-12 h-12" src={X} alt="Close" />
                         </div>
                     </div>
-                </div>
-                <div className="w-full flex justify-center mt-6">
-                    <div onClick={onClose} className="flex justify-center items-center h-12 w-12 bg-white rounded-full cursor-pointer">
-                        <img className="w-12 h-12" src={X} alt="Close" />
                     </div>
                 </div>
-            </div>
+                </div>
+
         </div>
+        </>
     );
 };
 
